e057fe3d1482beb8655869ab46d43f6f7919f02a
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-13 16:02:27 +0100

    Init java project

---
+++ tree_util/java/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/*
+./gradlew run
+./gradlew build
+java -jar build/libs/TreeUtil-1.0-SNAPSHOT.jar
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
