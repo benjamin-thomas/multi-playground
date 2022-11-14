63af65b7945b63d8213f5b8b7e1ebdf60f4146c9
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-14 08:45:48 +0100

    Tree util: add Java example

--- tree_util/java/src/main/java/org/example/Main.java
+++ tree_util/java/src/main/java/org/example/Main.java
@@ -1,12 +1,68 @@
 package org.example;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 /*
-./gradlew run
-./gradlew build
-java -jar build/libs/TreeUtil-1.0-SNAPSHOT.jar
+./gradlew run --args=~/code/explore/elm/turtle/
+
+./gradlew build && java -jar build/libs/TreeUtil-1.0-SNAPSHOT.jar ~/code/explore/elm/turtle/
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Must provide a path!");
+            System.exit(1);
+        }
+        var path = args[0];
+        walk(Paths.get(path));
+    }
+
+    private static void walk(Path root) throws IOException {
+        Files.walkFileTree(root, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                if (Files.isHidden(dir)) {
+                    return FileVisitResult.SKIP_SUBTREE;
+                }
+                if (dir != root) {
+                    printEntry(dir, attrs);
+                }
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (!Files.isHidden(file)) {
+                    printEntry(file, attrs);
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            private void printEntry(Path file, BasicFileAttributes attrs) {
+                boolean isDirectory = attrs.isDirectory();
+                long size = attrs.size();
+
+                Path simplePath = root.relativize(file);
+                int depth = simplePath.getNameCount() - 1;
+                var depthToWS = " ".repeat(depth);
+
+                System.out.printf("[%12s] %s [%3d] %s %s\n",
+                        toHuman(size), emoji(isDirectory), depth, depthToWS, simplePath);
+            }
+
+        });
+    }
+
+    private static String emoji(boolean isDirectory) {
+        return isDirectory ? "📁" : "📝";
+    }
+
+    private static String toHuman(long size) {
+        return FileUtils.byteCountToDisplaySize(size);
     }
 }
