d5766d62ecd37abe430d65a5c4db82a5a4b87135
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-16 07:56:45 +0100

    Monads: add another F# example

--- monads/fsharp/monads.fsx
+++ monads/fsharp/monads.fsx
@@ -8,6 +8,13 @@ let add x y =
     y >>= fun y ->
     Some (x + y)
 
+let bind_rev o fn = Option.bind fn o
+
+let mult x y =
+    bind_rev x (fun x -> 
+    bind_rev y (fun y ->
+    Some(x * y)))
+
 let printRes res =
     match res with
     | None -> printfn "None"
@@ -15,8 +22,13 @@ let printRes res =
 
 
 // dotnet fsi monads.fsx
-printf "\n--- MONADS EXAMPLE ---\n\n"
-printf "add (Some 1) (Some 2)      =>   ";; printRes <| add (Some 1) (Some 2);;
-printf "add (Some 1) None          =>   ";; printRes <| add (Some 1) None;;
-printf "add None     (Some 2)      =>   ";; printRes <| add None (Some 2);;
-printf "add None     None          =>   ";; printRes <| add None None;;
+printf "\n--- MONAD EXAMPLE ---\n\n"
+printf "add  (Some 1) (Some 2)     =>   ";; printRes <| add  (Some 1) (Some 2);;
+printf "add  (Some 1)  None        =>   ";; printRes <| add  (Some 1)  None;;
+printf "add   None    (Some 2)     =>   ";; printRes <| add   None    (Some 2);;
+printf "add   None     None        =>   ";; printRes <| add   None     None;;
+printf "---\n";;
+printf "mult (Some 2) (Some 3)     =>   ";; printRes <| mult (Some 2) (Some 3);;
+printf "mult  None    (Some 3)     =>   ";; printRes <| mult  None    (Some 3);;
+printf "mult (Some 2)  None        =>   ";; printRes <| mult (Some 2)  None;;
+printf "mult  None     None        =>   ";; printRes <| mult  None     None;;
