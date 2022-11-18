63b38e9c5790a19fbcf9046d241f8de214bfeb4c
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-18 23:28:00 +0100

    Add DB interaction example with F#

---
+++ db/fsharp/Program.fs
@@ -0,0 +1,61 @@
+﻿open FSharp.Data.Npgsql // https://github.com/demetrixbio/FSharp.Data.Npgsql
+
+(*
+    Initialize the database with:
+        ../init.ml
+
+    Build release fails if column has been renamed (good!)
+        dotnet build -c Release
+
+    PGUSER/PGPASSWORD env vars can sourced instead of providing the UserName/Password params below.
+*)
+[<Literal>]
+let connStr =
+    "Host=pg.hello.test;Database=mpg_db;UserName=postgres;Password=postgres;"
+
+type Db = NpgsqlConnection<connStr>
+
+let list_customers_freestyle =
+    use cmd =
+        Db.CreateCommand<"SELECT id, name, alternative_name AS alt_name FROM customers">(connStr)
+
+    for x in cmd.Execute() do
+
+        match x.alt_name with
+        | Some alt_name -> printfn "Customer #%04d -> %s (%s)" x.id x.name alt_name
+        | None -> printfn "Customer #%04d -> %s" x.id x.name
+
+(*
+type Customer =
+    {
+        Id: int
+        Name: string
+        AlternativeName: string option
+    }
+
+// CreateCommand returning a type we want to refer to in a function signature has to be 'mentioned' first
+let getAllCustomersCommand =
+    Db.CreateCommand<"SELECT id, name, alternative_name FROM customers">
+
+// The type with title and rating is now generated and accessible
+// error FS0039: The type 'id:String, name:String, alternative_name:Option<String>' is not defined.
+let mapCustomer (x: Db.``id:String, name:String, alternative_name:Option<String>``) : Customer =
+    {
+        Id = x.id
+        Name = x.name
+        AlternativeName = x.alternative_name
+    }
+
+let getAllCustomers () =
+    use cmd = getAllCustomersCommand connStr
+    let res = cmd.Execute()
+    res |> List.map mapCustomer
+*)
+
+[<EntryPoint>]
+let main (_) =
+    printfn "=== Testing database interactivity ==="
+
+    list_customers_freestyle
+
+    0
---
+++ db/init.ml
@@ -0,0 +1,66 @@
+#!/usr/bin/env utop
+
+(* `utop` gives me access to Unix, which I can't open with `ocaml` *)
+
+(* echo ./init.ml | entr -c /_ *)
+
+let dbName = "mpg_db"
+let createDb = Printf.sprintf "CREATE DATABASE %s;" dbName
+let dropDb = Printf.sprintf "DROP DATABASE %s;" dbName
+
+let createTables =
+  {|
+CREATE TABLE customers (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  , name VARCHAR(50) NOT NULL
+  , alternative_name VARCHAR(60) NULL
+);
+|}
+  |> String.trim
+;;
+
+let seedTables =
+  {|
+INSERT INTO customers (name, alternative_name) VALUES
+    ('John', 'Johnny')
+  , ('Mary', NULL)
+;
+|}
+  |> String.trim
+;;
+
+let log msg =
+  let tm = Unix.time () |> Unix.localtime in
+  Printf.printf
+    "[%04d-%d-%d %02d:%02d:%02d] => %s\n"
+    (tm.tm_year + 1900)
+    (tm.tm_mon + 1)
+    tm.tm_mday
+    tm.tm_hour
+    tm.tm_min
+    tm.tm_sec
+    msg
+;;
+
+let fail_on_err n = if n = 0 then () else failwith (Printf.sprintf "Exit code: %d" n)
+
+let psqlRoot cmd =
+  let relPath = Filename.dirname Sys.argv.(0) ^ "/psql_root" in
+  Filename.quote_command relPath [ "-qc"; cmd ]
+;;
+
+let psql cmd =
+  let relPath = Filename.dirname Sys.argv.(0) ^ "/psql" in
+  Filename.quote_command relPath [ "-qc"; cmd ]
+;;
+
+let _ =
+  log "Reset database!";
+  Sys.command (psqlRoot dropDb) |> ignore;
+  Sys.command (psqlRoot createDb) |> ignore;
+  log "Create tables";
+  Sys.command (psql createTables) |> fail_on_err;
+  log "Seed tables";
+  Sys.command (psql seedTables) |> fail_on_err;
+  log "Done!"
+;;
