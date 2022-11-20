71927fe1fa2194bb7d2a457d0e65a4712a8d6518
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-20 09:39:03 +0100

    FSharp.Data.Npgsql: map stuff

--- db/fsharp/FSharp.Data.Npgsql/Program.fs
+++ db/fsharp/FSharp.Data.Npgsql/Program.fs
@@ -15,7 +15,6 @@ let connStr =
 
 type Db = NpgsqlConnection<connStr, ReuseProvidedTypes=true>
 
-
 let list_customers_freestyle =
     use cmd =
         Db.CreateCommand<"SELECT id, name, alternative_name AS alt_name FROM customers">(connStr)
@@ -33,20 +32,6 @@ let test_list_tuples =
     let res: (int * string) list = cmd.Execute()
     res |> printfn "Customers as tuples: %A"
 
-let test_list_records =
-    use cmd =
-        Db.CreateCommand<"SELECT id, name FROM customers", ResultType.Records>(connStr)
-
-    let tmp = {| Id = 1; Name = "Ben" |}
-    // let res: List<Db.``id:Int32, name:String``> = cmd.Execute()
-    let res: (Db.``id:Int32, name:String``) list = cmd.Execute()
-    res |> printfn "Customers as records: %A"
-
-
-(*
--------------------------------------------
-FIXME: I could not get this code to compile
--------------------------------------------
 type Customer =
     {
         Id: int
@@ -54,24 +39,47 @@ type Customer =
         AlternativeName: string option
     }
 
-// CreateCommand returning a type we want to refer to in a function signature has to be 'mentioned' first
-let getAllCustomersCommand =
-    Db.CreateCommand<"SELECT id, name, alternative_name FROM customers">
+let test_list_records =
+    use cmd =
+        Db.CreateCommand<"SELECT id, name FROM customers WHERE id < @maxId LIMIT @limit", ResultType.Records>(connStr)
 
-// The type with title and rating is now generated and accessible
-// error FS0039: The type 'id:String, name:String, alternative_name:Option<String>' is not defined.
-let mapCustomer (x: Db.``id:String, name:String, alternative_name:Option<String>``) : Customer =
-    {
-        Id = x.id
-        Name = x.name
-        AlternativeName = x.alternative_name
-    }
+    let res: List<Db.``id:Int32, name:String``> =
+        // Use named params!
+        cmd.Execute(limit = 3, maxId = 10)
 
-let getAllCustomers () =
-    use cmd = getAllCustomersCommand connStr
-    let res = cmd.Execute()
-    res |> List.map mapCustomer
-*)
+    res |> printfn "Customers as records: %A"
+
+    // Code load order issue: this generated type is not available before `cmd.Execute()`
+    // So I can only define this function here for now.
+    let mapCustomer (x: Db.``id:Int32, name:String``) : Customer =
+        {
+            Id = x.id
+            Name = x.name
+            AlternativeName = None
+        }
+
+    res
+    |> List.map mapCustomer
+    |> printfn "Mapped customers: %A"
+
+let test_list_anon_records =
+    use cmd =
+        Db.CreateCommand<"SELECT id, name, alternative_name FROM customers WHERE id < @maxId LIMIT @limit", ResultType.Records>(
+            connStr
+        )
+
+    let res: List<Db.``alternative_name:Option<String>, id:Int32, name:String``> =
+        // Use named params!
+        cmd.Execute(limit = 3, maxId = 10)
+
+    res
+    |> List.map (fun c ->
+        {|
+            Id = c.id
+            Name = c.name
+            AlternativeName = c.alternative_name
+        |})
+
 
 [<EntryPoint>]
 let main (_) =
@@ -80,5 +88,11 @@ let main (_) =
     list_customers_freestyle
     test_list_tuples
     test_list_records
+
+    printfn "=> Using anonymous records..."
+
+    test_list_anon_records
+    |> List.iter (fun cust ->
+        printfn "ID      = %03d\nName    = %s\nAltName = %A\n" cust.Id cust.Name cust.AlternativeName)
 
     0
