180480d1de166aa757043f3f07ac69e796812874
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-20 10:40:43 +0100

    Init csharp project

---
+++ db/csharp/NpgsqlEfCore/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+dotnet ef dbcontext scaffold "Host=pg.mpg.test;Database=mpg_db;Username=postgres;Password=postgres" Npgsql.EntityFrameworkCore.PostgreSQL
+ */
+
+Console.WriteLine("Hello, World!");
