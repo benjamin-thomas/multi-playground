9af0518350f5c1e0c8df7d4c06ec63dccf0102d5
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-20 11:14:41 +0100

    Play with a simple query (fragile + abstraction is too constraining)

--- db/csharp/NpgsqlEfCore/Program.cs
+++ db/csharp/NpgsqlEfCore/Program.cs
@@ -5,4 +5,34 @@ dotnet ef dbcontext scaffold "Host=pg.mpg.test;Database=mpg_db;Username=postgres
 dotnet watch run NpgsqlEfCoreDemo
  */
 
-Console.WriteLine("Hello, World!");
+namespace NpgsqlEfCoreDemo;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=> Entity Framework Core demo...");
+
+        using var db = new mpg_dbContext();
+        var customers = db.Customers
+            .Where(b => b.Id < 10)
+            .OrderBy(b => b.Name) // how to order by nulls first/last?
+            .ToList();
+
+        foreach (var c in customers)
+        {
+            /*
+                I get this warning in Rider (line 35):
+                    "Dereference of a possibly null reference"
+                    
+                I can get an exception:
+                    "Unhandled exception. System.NullReferenceException:
+                         Object reference not set to an instance of an object."
+             */
+            Console.WriteLine(
+                $"ID      = {c.Id}\n" +
+                $"Name    = {c.Name} ({c.Name.Length})\n" +
+                $"AltName = {c.AlternativeName} ({c.AlternativeName.Length})\n");
+        }
+    }
+}
