2c762fc453da02ab014b4cf8f718e705bee73670
Benjamin <benjamin.guy.thomas@gmail.com>
2023-05-25 09:56:35 +0200

    Play with more monad constructs

--- monads/elm/tests/Monads.elm
+++ monads/elm/tests/Monads.elm
@@ -60,16 +60,27 @@ suite =
                     andMap : Result x a -> Result x (a -> b) -> Result x b
                     andMap =
                         Result.map2 (|>)
+
+                    -- https://nicolas.perriault.net/code/2021/elm-result-pipeline/
+                    apply : Result x a -> Result x (a -> b) -> Result x b
+                    apply result =
+                        Result.andThen (\partial -> Result.map partial result)
+
+                    apply2 x fn =
+                        Result.map fn x
                 in
-                Expect.equal (Ok 10)
-                    (Ok (\a b c d -> a + b + c + d)
+                Expect.equal (Ok 28)
+                    (Ok (\a b c d e f g -> a + b + c + d + e + f + g)
                         |> andMap (Ok 1)
                         |> andMap (Ok 2)
-                        |> andMap (Ok 3)
-                        |> andMap (Ok 4)
+                        |> apply (Ok 3)
+                        |> Result.andThen (\fn -> Result.map fn (Ok 4))
+                        |> Result.andThen (\fn -> Ok 5 |> Result.map fn)
+                        |> Result.andThen (apply2 (Ok 6))
+                        |> Result.map2 (\a b -> b a) (Ok 7)
                     )
         , test "Error is returned if the computation pipeline fails at any point" <|
-            \_ ->
+            \() ->
                 let
                     andMap : Result x a -> Result x (a -> b) -> Result x b
                     andMap =
@@ -83,4 +94,57 @@ suite =
                         |> andMap (Err "Oops @3")
                         |> andMap (Ok 4)
                     )
+        , test "let's apply the elm-result-pipeline style to Maybe" <|
+            \() ->
+                let
+                    apply : Maybe a -> Maybe (a -> b) -> Maybe b
+                    apply m =
+                        Maybe.andThen (\partial -> Maybe.map partial m)
+                in
+                Expect.equal (Just 13)
+                    (Just (\a b c -> a * b + c)
+                        |> apply (Just 3)
+                        |> apply (Just 4)
+                        |> apply (Just 1)
+                    )
+        , test "this method processes the args in a more logical order (see `andMap` above)" <|
+            \_ ->
+                let
+                    -- https://nicolas.perriault.net/code/2021/elm-result-pipeline/
+                    apply : Result x a -> Result x (a -> b) -> Result x b
+                    apply result =
+                        Result.andThen (\partial -> Result.map partial result)
+                in
+                Expect.equal (Err "2")
+                    (Ok (\a b c d -> a + b + c + d)
+                        |> apply (Ok 1)
+                        |> apply (Err "2")
+                        |> apply (Err "3")
+                        |> apply (Ok 4)
+                    )
+        , test "verbose style" <|
+            \() ->
+                let
+                    myComputation =
+                        Ok (\a b c -> a + b + c)
+                            |> Result.andThen (\fn -> Result.map fn (Ok 1))
+                            |> Result.andThen (\fn -> Result.map fn (Ok 2))
+                            |> Result.andThen (\fn -> Result.map fn (Ok 3))
+                in
+                Expect.equal (Ok 6) myComputation
+        , test "in verbose style, the computation does stop on the first error" <|
+            \() ->
+                Expect.equal (Err "2")
+                    (Ok (\a b c -> a + b + c)
+                        |> Result.andThen (\fn -> Result.map fn (Ok 1))
+                        |> Result.andThen (\fn -> Result.map fn (Err "2"))
+                        |> Result.andThen (\fn -> Result.map fn (Err "3"))
+                    )
+        , test "simplify" <|
+            \() ->
+                let
+                    myComputation =
+                        Ok identity |> Result.andThen (\fn -> Result.map fn (Ok 1))
+                in
+                Expect.equal (Ok 1) myComputation
         ]
--- monads/ocaml/monads.ml
+++ monads/ocaml/monads.ml
@@ -1,16 +1,21 @@
-(* echo monads.ml | entr -c bash -c 'ocamlc ./monads.ml && ./a.out' *)
-(* echo monads.ml | entr -c ocaml /_ *)
+(* Use the compiler
+ *   echo monads.ml | entr -c bash -c 'ocamlc ./monads.ml && ./a.out'
+ *)
+
+(* Or use the top-level interactive mode
+ * echo monads.ml | entr -c ocaml /_
+ *)
 
 let add_a x y z =
   match x with
   | None -> None
-  | Some x_ ->
-    (match y with
-     | None -> None
-     | Some y_ ->
-       (match z with
-        | None -> None
-        | Some z_ -> Some (x_ + y_ + z_)))
+  | Some x_ -> (
+      match y with
+      | None -> None
+      | Some y_ -> (
+          match z with
+          | None -> None
+          | Some z_ -> Some (x_ + y_ + z_)))
 ;;
 
 let add_b x y z =
@@ -21,10 +26,10 @@ let add_b x y z =
       )
     )
   )
-    [@@ocamlformat "disable"]
+  [@@ocamlformat "disable"]
 
 let add_c x y z =
-  match x, y, z with
+  match (x, y, z) with
   | Some x, Some y, Some z -> Some (x + y + z)
   | _ -> None
 ;;
@@ -57,7 +62,7 @@ let add_e x y z =
  Rather than using `and_map`, I'll use the function name `apply` (it seems fit?)
  *)
 let apply opt_a opt_b =
-  match opt_a, opt_b with
+  match (opt_a, opt_b) with
   | Some value, Some fn -> Some (fn value)
   | _, _ -> None
 ;;
@@ -67,21 +72,43 @@ let add_f a b c =
     |> apply a
     |> apply b
     |> apply c
-;;
+  [@@ocamlformat "disable"]
 
 let apply res_a res_b =
-  match res_a, res_b with
+  match (res_a, res_b) with
   | Ok value, Ok fn -> Ok (fn value)
   | Error err, _ -> Error err
   | _, Error err -> Error err
 ;;
 
 let add_g a b c =
-    Ok (fun x y z -> x + y + z)
+  Ok (fun x y z -> x + y + z)
     |> apply a
     |> apply b
     |> apply c
+  [@@ocamlformat "disable"]
+
+(* See again the Elm examples *)
+
+let apply' res_a res_b =
+  Result.bind res_b (fun partial -> Result.map partial res_a)
 ;;
+
+let add_h a b c =
+  Ok (fun x y z -> x + y + z)
+   |> apply' a
+   |> apply' b
+   |> apply' c
+  [@@ocamlformat "disable"]
+
+let and_then a b = Result.bind b a
+
+let add_i a b c =
+   Ok (fun x y z -> x + y + z)
+     |> and_then (fun partial -> Result.map partial a)
+     |> and_then (fun partial -> Result.map partial b)
+     |> and_then (fun partial -> Result.map partial c)
+   [@@ocamlformat "disable"]
 
 (* MAIN *)
 
@@ -99,79 +126,121 @@ let printRes2 header fn =
 
 let _ =
   (* add_a: verbose! *)
-  printRes "add_a (Some 1) (Some 2) (Some 3)" @@ add_a (Some 1) (Some 2) (Some 3);
-  printRes "add_a None (Some 2) (Some 3)    " @@ add_a None (Some 2) (Some 3);
-  printRes "add_a (Some 1) None (Some 3)    " @@ add_a (Some 1) None (Some 3);
-  printRes "add_a (Some 1) None None        " @@ add_a (Some 1) None (Some 3);
-  print_endline "";
-  (* add_b: fn chain *)
-  printRes "add_b (Some 1) (Some 2) (Some 3)" @@ add_b (Some 1) (Some 2) (Some 3);
-  printRes "add_b None (Some 2) (Some 3)    " @@ add_b None (Some 2) (Some 3);
-  printRes "add_b (Some 1) None (Some 3)    " @@ add_b (Some 1) None (Some 3);
-  printRes "add_b (Some 1) None None        " @@ add_b (Some 1) None (Some 3);
-  print_endline "";
-  (* add_c: use pattern matching *)
-  printRes "add_c (Some 1) (Some 2) (Some 3)" @@ add_c (Some 1) (Some 2) (Some 3);
-  printRes "add_c None (Some 2) (Some 3)    " @@ add_c None (Some 2) (Some 3);
-  printRes "add_c (Some 1) None (Some 3)    " @@ add_c (Some 1) None (Some 3);
-  printRes "add_c (Some 1) None None        " @@ add_c (Some 1) None (Some 3);
-  print_endline "";
-  (* add_d: use let bind *)
-  printRes "add_d (Some 1) (Some 2) (Some 3)" @@ add_d (Some 1) (Some 2) (Some 3);
-  printRes "add_d None (Some 2) (Some 3)    " @@ add_d None (Some 2) (Some 3);
-  printRes "add_d (Some 1) None (Some 3)    " @@ add_d (Some 1) None (Some 3);
-  printRes "add_d (Some 1) None None        " @@ add_d (Some 1) None (Some 3);
-  print_endline "";
-  (* add_e: use bind operator, Haskell style *)
-  printRes "add_e (Some 1) (Some 2) (Some 3)" @@ add_e (Some 1) (Some 2) (Some 3);
-  printRes "add_e None (Some 2) (Some 3)    " @@ add_e None (Some 2) (Some 3);
-  printRes "add_e (Some 1) None (Some 3)    " @@ add_e (Some 1) None (Some 3);
-  printRes "add_e (Some 1) None None        " @@ add_e (Some 1) None (Some 3);
-  print_endline "";
-  (* add_f: Elm style *)
-  printRes "add_f (Some 1) (Some 2) (Some 3)" @@ add_f (Some 1) (Some 2) (Some 3);
-  printRes "add_f None (Some 2) (Some 3)    " @@ add_f None (Some 2) (Some 3);
-  printRes "add_f (Some 1) None (Some 3)    " @@ add_f (Some 1) None (Some 3);
-  printRes "add_f (Some 1) None None        " @@ add_f (Some 1) None (Some 3);
-  print_endline "";
-  (* add_g: Elm style. NOTE: as with Elm, the arguments are processed in reverse. *)
-  printRes2 "add_g (Ok 1) (Ok 2) (Ok 3)                  " @@ add_g (Ok 1) (Ok 2) (Ok 3);
-  printRes2 "add_g (Error 'Oops1') (Ok 2) (Ok 3)         " @@ add_g (Error "Oops1") (Ok 2) (Ok 3);
-  printRes2 "add_g (Ok 1) (Error 'Oops2') (Ok 3)         " @@ add_g (Ok 1) (Error "Oops2") (Ok 3);
-  printRes2 "add_g (Ok 1) (Error 'Oops2') (Error 'Oops3')" @@ add_g (Ok 1) (Error "Oops2") (Error "Oops3");
-  print_endline "";
+  printRes "add_a (Some 1) (Some 2) (Some 3)"
+  @@ add_a (Some 1) (Some 2) (Some 3)
+  ; printRes "add_a None (Some 2) (Some 3)    " @@ add_a None (Some 2) (Some 3)
+  ; printRes "add_a (Some 1) None (Some 3)    " @@ add_a (Some 1) None (Some 3)
+  ; printRes "add_a (Some 1) None None        " @@ add_a (Some 1) None (Some 3)
+  ; print_endline ""
+  ; (* add_b: fn chain *)
+    printRes "add_b (Some 1) (Some 2) (Some 3)"
+    @@ add_b (Some 1) (Some 2) (Some 3)
+  ; printRes "add_b None (Some 2) (Some 3)    " @@ add_b None (Some 2) (Some 3)
+  ; printRes "add_b (Some 1) None (Some 3)    " @@ add_b (Some 1) None (Some 3)
+  ; printRes "add_b (Some 1) None None        " @@ add_b (Some 1) None (Some 3)
+  ; print_endline ""
+  ; (* add_c: use pattern matching *)
+    printRes "add_c (Some 1) (Some 2) (Some 3)"
+    @@ add_c (Some 1) (Some 2) (Some 3)
+  ; printRes "add_c None (Some 2) (Some 3)    " @@ add_c None (Some 2) (Some 3)
+  ; printRes "add_c (Some 1) None (Some 3)    " @@ add_c (Some 1) None (Some 3)
+  ; printRes "add_c (Some 1) None None        " @@ add_c (Some 1) None (Some 3)
+  ; print_endline ""
+  ; (* add_d: use let bind *)
+    printRes "add_d (Some 1) (Some 2) (Some 3)"
+    @@ add_d (Some 1) (Some 2) (Some 3)
+  ; printRes "add_d None (Some 2) (Some 3)    " @@ add_d None (Some 2) (Some 3)
+  ; printRes "add_d (Some 1) None (Some 3)    " @@ add_d (Some 1) None (Some 3)
+  ; printRes "add_d (Some 1) None None        " @@ add_d (Some 1) None (Some 3)
+  ; print_endline ""
+  ; (* add_e: use bind operator, Haskell style *)
+    printRes "add_e (Some 1) (Some 2) (Some 3)"
+    @@ add_e (Some 1) (Some 2) (Some 3)
+  ; printRes "add_e None (Some 2) (Some 3)    " @@ add_e None (Some 2) (Some 3)
+  ; printRes "add_e (Some 1) None (Some 3)    " @@ add_e (Some 1) None (Some 3)
+  ; printRes "add_e (Some 1) None None        " @@ add_e (Some 1) None (Some 3)
+  ; print_endline ""
+  ; (* add_f: Elm style *)
+    printRes "add_f (Some 1) (Some 2) (Some 3)"
+    @@ add_f (Some 1) (Some 2) (Some 3)
+  ; printRes "add_f None (Some 2) (Some 3)    " @@ add_f None (Some 2) (Some 3)
+  ; printRes "add_f (Some 1) None (Some 3)    " @@ add_f (Some 1) None (Some 3)
+  ; printRes "add_f (Some 1) None None        " @@ add_f (Some 1) None (Some 3)
+  ; print_endline ""
+  ; (* add_g: Elm style. NOTE: as with Elm, the arguments are processed in reverse. *)
+    printRes2 "add_g (Ok 1) (Ok 2) (Ok 3)                  " @@ add_g (Ok 1) (Ok 2) (Ok 3)
+  ; printRes2 "add_g (Error 'Oops1') (Ok 2) (Ok 3)         " @@ add_g (Error "Oops1") (Ok 2) (Ok 3)
+  ; printRes2 "add_g (Ok 1) (Error 'Oops2') (Ok 3)         " @@ add_g (Ok 1) (Error "Oops2") (Ok 3)
+  ; printRes2 "add_g (Ok 1) (Error 'Oops2') (Error 'Oops3')" @@ add_g (Ok 1) (Error "Oops2") (Error "Oops3")
+  ; print_endline ""
+  ; (* add_h: Elm style again. This fixes the arguments being processed in reverse order. *)
+    printRes2 "add_h (Ok 1) (Ok 2) (Ok 3)                  " @@ add_h (Ok 1) (Ok 2) (Ok 3)
+  ; printRes2 "add_h (Error 'Oops1') (Ok 2) (Ok 3)         " @@ add_h (Error "Oops1") (Ok 2) (Ok 3)
+  ; printRes2 "add_h (Ok 1) (Error 'Oops2') (Ok 3)         " @@ add_h (Ok 1) (Error "Oops2") (Ok 3)
+  ; printRes2 "add_h (Ok 1) (Error 'Oops2') (Error 'Oops3')" @@ add_h (Ok 1) (Error "Oops2") (Error "Oops3")
+  ; print_endline ""
+  (* add_h: Elm style again, but verbose mode. It processes the arguments being processed in the correct order.
+   * Note that I had to create a custom Result.bind (and_then) with a reversed order to make that work.
+   *)
+  ; printRes2 "add_i (Ok 1) (Ok 2) (Ok 3)                  " @@ add_i (Ok 1) (Ok 2) (Ok 3)
+  ; printRes2 "add_i (Error 'Oops1') (Ok 2) (Ok 3)         " @@ add_i (Error "Oops1") (Ok 2) (Ok 3)
+  ; printRes2 "add_i (Ok 1) (Error 'Oops2') (Ok 3)         " @@ add_i (Ok 1) (Error "Oops2") (Ok 3)
+  ; printRes2 "add_i (Ok 1) (Error 'Oops2') (Error 'Oops3')" @@ add_i (Ok 1) (Error "Oops2") (Error "Oops3")
+  ; print_endline ""
+[@@ocamlformat "disable"]
+
+let succeed b =
+  if b then
+    Ok 1
+  else
+    Error "Oops!"
 ;;
 
-let succeed b = if b then Ok 1 else Error "Oops!"
+let ( let* ) = Result.bind
 
-let (let*) = Result.bind
+let compute_ok_ok =
+  let* a = succeed true in
+  let* b = succeed true in
+  Ok (a + b)
+;;
 
-let compute_ok_ok  =
-    let* a = succeed true in
-    let* b = succeed true in
-    Ok (a + b)
-
-let compute_ok_err  =
+let compute_ok_err =
   let* a = succeed true in
   let* b = succeed false in
   Ok (a + b)
+;;
 
 let print_my_result = function
   | Error s -> s
   | Ok n -> string_of_int n
+;;
 
 (** returns either Ok, otherwise the Error of the same type
  *  This is somewhat unusual I suppose, I just want to print the original error below.
  *  I could also use the `print_result` function above.
  *)
-let ok_or_err r = match r with Ok v -> v | Error e -> e
+let ok_or_err r =
+  match r with
+  | Ok v -> v
+  | Error e -> e
+;;
 
 let () =
   ()
-  ; Printf.printf "a) Add with (Ok, Ok): %s\n%!"  (compute_ok_ok  |> Result.map (fun n -> string_of_int n) |> Result.value ~default:"oops")
-  ; Printf.printf "a) Add with (Ok, Err): %s\n%!" (compute_ok_err |> Result.map (fun n -> string_of_int n) |> Result.value ~default:"oops")
-  ; Printf.printf "b) Add with (Ok, Ok): %s\n%!"  (compute_ok_ok  |> Result.map (fun n -> string_of_int n) |> ok_or_err)
-  ; Printf.printf "b) Add with (Ok, Err): %s\n%!" (compute_ok_err |> Result.map (fun n -> string_of_int n) |> ok_or_err)
-  ; Printf.printf "c) Add with (Ok, Ok): %s\n%!"  (compute_ok_ok  |> print_my_result)
-  ; Printf.printf "c) Add with (Ok, Err): %s\n%!" (compute_ok_err |> print_my_result)
-
+  ; Printf.printf "a) Add with (Ok, Ok): %s\n%!"
+      (compute_ok_ok
+      |> Result.map (fun n -> string_of_int n)
+      |> Result.value ~default:"oops")
+  ; Printf.printf "a) Add with (Ok, Err): %s\n%!"
+      (compute_ok_err
+      |> Result.map (fun n -> string_of_int n)
+      |> Result.value ~default:"oops")
+  ; Printf.printf "b) Add with (Ok, Ok): %s\n%!"
+      (compute_ok_ok |> Result.map (fun n -> string_of_int n) |> ok_or_err)
+  ; Printf.printf "b) Add with (Ok, Err): %s\n%!"
+      (compute_ok_err |> Result.map (fun n -> string_of_int n) |> ok_or_err)
+  ; Printf.printf "c) Add with (Ok, Ok): %s\n%!"
+      (compute_ok_ok |> print_my_result)
+  ; Printf.printf "c) Add with (Ok, Err): %s\n%!"
+      (compute_ok_err |> print_my_result)
+;;
