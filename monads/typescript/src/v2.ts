ea28b7fc37d7caadb921c215df208aca0c6ee78a
Benjamin <benjamin.guy.thomas@gmail.com>
2023-06-03 20:44:43 +0200

    Add map2, Elm style!

--- monads/typescript/src/v2.ts
+++ monads/typescript/src/v2.ts
@@ -17,17 +17,41 @@ export const some = function <A>(a: A): Option<A> {
 function bind<A, B>(a: Option<A>,
                     fn: (a: A) => Option<B>): Option<B> {
     if (a.kind === 'None') {
-        return none
+        return none;
     } else {
-        return fn(a.val)
+        return fn(a.val);
     }
 }
+
+function map2<A, B, V>(
+    fn: (a: A, b: B) => V,
+    a: Option<A>,
+    b: Option<B>)
+    : Option<V> {
+    if (a.kind === 'None') {
+        return none;
+    } else {
+        if (b.kind === 'None') {
+            return none;
+        } else {
+            return some(fn(a.val, b.val));
+        }
+    }
+}
+
 
 export function add(a: Option<number>, b: Option<number>): Option<number> {
     return bind(a, (x) => {
         return bind(b, (y) => {
             return some(x + y);
-        })
-    })
+        });
+    });
 }
+
+export function mul(a: Option<number>, b: Option<number>): Option<number> {
+    return map2(
+        (x, y) => x * y,
+        a, b);
+}
+
 
--- monads/typescript/tests/v2.test.ts
+++ monads/typescript/tests/v2.test.ts
@@ -1,4 +1,4 @@
-import {some, none, add} from "../src/v2";
+import { some, none, add, mul } from "../src/v2";
 
 describe('v2', () => {
     it('add', () => {
@@ -17,5 +17,12 @@ describe('v2', () => {
         expect(
             add(some(1), some(2))
         ).toEqual(some(3));
+    });
+
+    it('multiplies, Elm style!', () => {
+        expect(mul(none, none)).toEqual(none);
+        expect(mul(some(3), none)).toEqual(none);
+        expect(mul(none, some(4))).toEqual(none);
+        expect(mul(some(3), some(4))).toEqual(some(12));
     });
 });
