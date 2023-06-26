c6899705c92c61af620320ea55a80c520fe806c7
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2023-06-26 08:47:12 +0200

    WIP: tests+find_by_id

--- db/ocaml/caqti_sqlite/lib/repo/customer.ml
+++ db/ocaml/caqti_sqlite/lib/repo/customer.ml
@@ -29,6 +29,15 @@ module Q = struct
     |}
   ;;
 
+  let find_by_id =
+    Caqti_type.(int ->! string)
+    @@ {|
+    SELECT first_name
+    FROM customers
+    WHERE id = ?
+    |}
+  ;;
+
   let update =
     Caqti_type.(tup3 int string string ->. unit)
     @@ {|
@@ -61,8 +70,56 @@ let insert (module Conn : Caqti_lwt.CONNECTION) first_name last_name =
   Conn.exec Q.insert (first_name, last_name)
 ;;
 
-let delete (module Conn : Caqti_lwt.CONNECTION) id = Conn.exec Q.delete id
+let find_by_id (module Conn : Caqti_lwt.CONNECTION) id =
+  Conn.find Q.find_by_id id
+;;
+
+let find_by_id' (module Conn : Caqti_lwt.CONNECTION) id =
+  let open Lwt.Syntax in
+  let* res = Conn.find Q.find_by_id id in
+  Lwt.return (Result.map_error Caqti_error.show res)
+;;
+
+[@@@warning "-33-26"]
+
+let%test_unit "find_by_id" =
+  let ( => ) = [%test_eq: (Base.string, Base.string) Base.Result.t] in
+  (* let open Lwt_result.Syntax in *)
+  let open Lwt.Syntax in
+  let conn = Init.caqti_conn () in
+  (* let find_by_id = find_by_id' conn in *)
+  let prom = find_by_id' conn 1 in
+  match (Lwt_main.run prom ) with
+  | Error _ -> failwith "wat"
+  | Ok x ->
+    (
+
+      Ok x => Ok "1"
+      )
+  (* ()
+  ; find_by_id 1  => Ok "John"
+  ; find_by_id 1  => Ok "John2" *)
+[@@ocamlformat "disable"]
+
+(*
+ dune runtest -w
+
+ FIXME: db is read in a _build/.sandbox/{RANDOM} folder
+ *)
+let%test_unit "find_by_id2" =
+  let ( => ) = [%test_eq: (Base.string, Base.string) Base.Result.t] in
+  let open Lwt_result.Syntax in
+  let conn = Init.caqti_conn () in
+  (* let find_by_id = find_by_id' conn in *)
+  let prom = find_by_id' conn 1 in
+  Lwt_main.run prom => Ok "John"
+  (* ()
+  ; find_by_id 1  => Ok "John"
+  ; find_by_id 1  => Ok "John2" *)
+[@@ocamlformat "disable"]
 
 let update (module Conn : Caqti_lwt.CONNECTION) id (c : customer) =
   Conn.exec Q.update (id, c.first_name, c.last_name)
 ;;
+
+let delete (module Conn : Caqti_lwt.CONNECTION) id = Conn.exec Q.delete id
