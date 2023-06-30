4b8706275de8f2f1b3986c15f004026b9efa44d9
Benjamin <benjamin.guy.thomas@gmail.com>
2023-06-30 23:27:57 +0200

    Move tests

--- db/ocaml/caqti_sqlite/lib/repo/customer.ml
+++ db/ocaml/caqti_sqlite/lib/repo/customer.ml
@@ -81,29 +81,6 @@ let find_by_id (module Conn : Caqti_lwt.CONNECTION) id =
   Conn.find Q.find_by_id id
 ;;
 
-let fail_setup e = failwith @@ "test setup failed: " ^ Caqti_error.show e
-
-let%test_unit "find_by_id" =
-  let ( => ) = [%test_eq: (Base.string, Base.string) Base.Result.t] in
-  let open Lwt_result.Syntax in
-  let conn = Init.caqti_conn () in
-  let setup : (unit, 'error) result Lwt.t =
-    let* () = create_tbl conn () in
-    let* () = insert conn "John" "Doe" in
-    Lwt.return_ok ()
-  in
-  match Lwt_main.run setup with
-  | Error e -> fail_setup e
-  | Ok () ->
-      let prom =
-        let open Lwt.Syntax in
-        let* res = find_by_id conn 1 in
-        let res = Result.map_error Caqti_error.show res in
-        Lwt.return res
-      in
-      Lwt_main.run prom => Ok "John"
-;;
-
 let update (module Conn : Caqti_lwt.CONNECTION) id (c : customer) =
   Conn.exec Q.update (id, c.first_name, c.last_name)
 ;;
@@ -117,56 +94,3 @@ let delete (module Conn : Caqti_lwt.CONNECTION) id = Conn.exec Q.delete id
    utop # Customer.count conn ();;
  *)
 let count (module Conn : Caqti_lwt.CONNECTION) = Conn.find Q.count
-
-let test_will_count conn =
-  let open Lwt.Syntax in
-  let* res = count conn () in
-  let res = Result.map_error Caqti_error.show res in
-  Lwt.return res
-;;
-
-let test_will_insert_then_count conn =
-  let open Lwt.Syntax in
-  let* ins_res = insert conn "Jane" "Doe" in
-  let* cnt_res = count conn () in
-  let res =
-    Result.bind ins_res (fun () -> cnt_res) |> Result.map_error Caqti_error.show
-  in
-  Lwt.return res
-;;
-
-let init_test_db () =
-  let conn = Init.caqti_conn () in
-  let setup : (unit, 'error) result Lwt.t =
-    let open Lwt_result.Syntax in
-    let* () = create_tbl conn () in
-    Lwt.return_ok ()
-  in
-  (conn, setup)
-;;
-
-let%test_unit "count returns 1 since, John already exists" =
-  let ( => ) = [%test_eq: (Base.int, Base.string) Base.Result.t] in
-  (* GIVEN *)
-  let conn, setup = init_test_db () in
-  match Lwt_main.run setup with
-  | Error e -> fail_setup e
-  | Ok () ->
-      (* WHEN *)
-      let prom = test_will_count conn in
-      (* THEN *)
-      Lwt_main.run prom => Ok 1
-;;
-
-let%test_unit "count returns 2, after inserting Jane" =
-  let ( => ) = [%test_eq: (Base.int, Base.string) Base.Result.t] in
-  (* GIVEN *)
-  let conn, setup = init_test_db () in
-  match Lwt_main.run setup with
-  | Error e -> fail_setup e
-  | Ok () ->
-      (* WHEN *)
-      let prom = test_will_insert_then_count conn in
-      (* THEN *)
-      Lwt_main.run prom => Ok 2
-;;
