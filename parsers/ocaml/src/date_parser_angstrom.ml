a24b242433a156700d06dc7e4597724f3b58e28f
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2023-08-17 07:55:34 +0200

    Play with Angstrom

--- parsers/ocaml/src/date_parser_angstrom.ml
+++ parsers/ocaml/src/date_parser_angstrom.ml
@@ -14,6 +14,75 @@ let digits : int A.t =
   >>| int_of_string
 ;;
 
+module Parser = Angstrom
+
+let digits2 : int Parser.t =
+  let take_digits =
+    Parser.take_while1 (function
+      | '0' .. '9' -> true
+      | _ -> false)
+  in
+  Parser.map ~f:int_of_string take_digits
+;;
+
+let digits3 : int Parser.t =
+  let take_digits =
+    Parser.take_while1 (function
+      | '0' .. '9' -> true
+      | _ -> false)
+  in
+  Parser.bind
+    ~f:(fun x ->
+      match int_of_string_opt x with
+      | Some n -> Parser.return n
+      | None -> Parser.fail "impossible")
+    take_digits
+;;
+
+(** [fromOption] assumes that applying its function parameter will never return [None].
+ * If it does, the parsing fails.
+ *)
+let from_option = function
+  | Some n -> Parser.return n
+  | None -> Parser.fail "impossible"
+;;
+
+let ( >> ) f g x = f (g x)
+
+let digits4 : int Parser.t =
+  let take_digits =
+    Parser.take_while1 (function
+      | '0' .. '9' -> true
+      | _ -> false)
+  in
+  Parser.bind ~f:(from_option >> int_of_string_opt) take_digits
+;;
+
+let digits5 : int Parser.t =
+  let take_digits =
+    Parser.take_while1 (function
+      | '0' .. '9' -> true
+      | _ -> false)
+  in
+  let f s = from_option (int_of_string_opt s) in
+  Parser.bind ~f take_digits
+;;
+
+let digits6 : int Parser.t =
+  let take_digits =
+    Parser.take_while1 (function
+      | '0' .. '9' -> true
+      | _ -> false)
+  in
+
+  let to_int s =
+    int_of_string_opt s |> function
+    | Some n -> Parser.return n
+    | None -> Parser.fail "there must be a bug in take_digits (!??)"
+  in
+  Parser.bind ~f:to_int take_digits
+;;
+
 (* chomp exactly `n` digits *)
 let digits' n =
   A.take n >>= fun s ->
