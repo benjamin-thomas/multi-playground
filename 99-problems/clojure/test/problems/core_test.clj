93a9b9fbf5fc02cd4867396d1ea2bb7c0229d628
Benjamin <benjamin.guy.thomas@gmail.com>
2023-09-07 19:04:44 +0200

    Update stack overflow condition

--- 99-problems/clojure/test/problems/core_test.clj
+++ 99-problems/clojure/test/problems/core_test.clj
@@ -6,8 +6,9 @@
   (is (= 3 (my-last [1 2 3])))
   (is (= 6 (my-last '(4 5 6))))
 
-  ; I overflow the stack with 5200 items here. For comparaison, Ruby overflows with 8800 items with a similar function.
-  (is (= 5099 (my-last (range 0 5100))))
+  ; I overflow the stack with 5100 items here. For comparaison, Ruby overflows with 8800 items with a similar function.
+  ; NOTE: I overflowed the stack with 4900 items on another computer.
+  (is (= 4799 (my-last (range 0 4800))))
   )
 
 (deftest my-last2-test
