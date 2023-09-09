9050f02542e98e36774ddcedaaf5e1f9b18b8a4f
Benjamin <benjamin.guy.thomas@gmail.com>
2023-09-09 11:23:32 +0200

    Up to problem 3

--- 99-problems/ocaml/lists.ml
+++ 99-problems/ocaml/lists.ml
@@ -8,7 +8,11 @@ dune utop
 
 *)
 
-(* Write a function last : 'a list -> 'a option that returns the last element of a list *)
+open Printf
+
+(*
+ * 1) Write a function last : 'a list -> 'a option that returns the last element of a list
+ *)
 let rec last = function
   | [] -> None
   | x :: [] -> Some x
@@ -28,4 +32,74 @@ let%expect_test _ =
   ; ()
   ; print @@ last [ 1; 2 ]
   ; [%expect "2"]
+;;
+
+(*
+ * 2) Find the last but one (last and penultimate) elements of a list.
+ *)
+let rec last_two lst : ('a * 'b) option =
+  match lst with
+  | [] -> None
+  | [ a; b ] -> Some (a, b)
+  | _ :: t -> last_two t
+;;
+
+let%expect_test _ =
+  let print = function
+    | None -> printf "None"
+    | Some (a, b) -> printf {|Some ("%s", "%s")|} a b
+  in
+  ()
+  ; print @@ last_two [ "a"; "b"; "c"; "d" ]
+  ; [%expect {|Some ("c", "d")|}]
+  ; print @@ last_two [ "a" ]
+  ; [%expect {| None |}]
+  ; print @@ last_two []
+  ; [%expect {| None |}]
+;;
+
+(*
+ * 3) Find the N'th element of a list.
+ *)
+
+let nth lst n =
+  let rec loop n lst =
+    match (n, lst) with
+    | 0, h :: _ -> h
+    | _, [] -> failwith "nth"
+    | _, _ :: t -> loop (n - 1) t
+  in
+  loop n lst
+;;
+
+let%expect_test _ =
+  ()
+  ; print_string @@ nth [ "a"; "b"; "c"; "d"; "e" ] 2
+  ; [%expect {| c |}]
+  ; print_string
+      (try nth [ "a" ] 2 with
+      | _ -> "FAILED")
+  ; [%expect "FAILED"]
+;;
+
+let rec at lst n =
+  match lst with
+  | [] -> None
+  | h :: t ->
+      if n > 0 then
+        at t (n - 1)
+      else
+        Some h
+;;
+
+let%expect_test _ =
+  let print = function
+    | None -> print_string "None"
+    | Some x -> printf {|Some "%s"|} x
+  in
+  ()
+  ; print @@ at [ "a"; "b"; "c"; "d"; "e" ] 2
+  ; [%expect {| Some "c" |}]
+  ; print @@ at [ "a" ] 2
+  ; [%expect "None"]
 ;;
