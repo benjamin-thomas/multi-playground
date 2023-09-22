b937d4d0d567a041caa79cfd88c08718baa97bf5
Benjamin <benjamin.guy.thomas@gmail.com>
2023-09-22 08:35:51 +0200

    Simplify workflow

--- 99-problems/typescript/main_test.ts
+++ 99-problems/typescript/main_test.ts
@@ -1,18 +1,17 @@
-import {describe, expect, test} from "bun:test";
+import {expect, test} from "bun:test";
 
 /*
-    bun test --watch
-*/
+ *   bun test --watch
+ */
 
-// 1 - Return the last element of a list
-describe("Suite", () => {
+/*
+ * 01 - Return the last element of a list"
+ */
+function last<T>(lst: Array<T>): T {
+    return lst[lst.length - 1];
+}
 
-    test("Exercise 01", () => {
-        function last<T>(lst: Array<T>): T {
-            return lst[lst.length - 1];
-        }
-
-        expect(last([])).toBe(undefined);
-        expect(last([1, 2, 3])).toBe(3);
-    });
+test("01", () => {
+    expect(last([])).toBe(undefined);
+    expect(last([1, 2, 3])).toBe(3);
 });
