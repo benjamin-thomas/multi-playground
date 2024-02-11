2cdb7e3c3b3048a35d5c56c2a4197f5c5bd837f3
Benjamin <benjamin.guy.thomas@gmail.com>
2024-02-11 10:18:07 +0100

    Add itemA parser

--- parsers/elm/tests/SpecialCodeParser.elm
+++ parsers/elm/tests/SpecialCodeParser.elm
@@ -1,10 +1,5 @@
 module SpecialCodeParser exposing (..)
 
-{-
-
-   WIP: I'm kinda clueless here!
--}
-
 import Expect
 import Parser as P
     exposing
@@ -48,11 +43,67 @@ testListParser =
         ]
 
 
+type Repeat
+    = Repeat Int
+
+
 type alias SpecialCode =
-    { repeat : Int
+    { repeat : Repeat
     , notice : String
     , alwaysRun : Bool
     }
+
+
+repeatParser : Parser Repeat
+repeatParser =
+    P.succeed Repeat
+        |. P.symbol "repeat="
+        |= P.int
+
+
+repeatParserTest : Test
+repeatParserTest =
+    let
+        test_ n ( inp, exp ) =
+            test ("parse repeat #" ++ String.fromInt n) <|
+                \() -> P.run repeatParser inp |> Expect.equal exp
+    in
+    describe "repeatParserTest" <|
+        List.indexedMap test_ <|
+            [ ( ""
+              , Err [ { col = 1, problem = P.ExpectingSymbol "repeat=", row = 1 } ]
+              )
+            , ( "repeat=123"
+              , Ok (Repeat 123)
+              )
+            , ( "repeaT=123"
+              , Err [ { col = 1, problem = P.ExpectingSymbol "repeat=", row = 1 } ]
+              )
+            , ( "wat=123"
+              , Err [ { col = 1, problem = P.ExpectingSymbol "repeat=", row = 1 } ]
+              )
+            , ( "repeat==123"
+              , Err [ { col = 8, problem = P.ExpectingInt, row = 1 } ]
+              )
+            , ( "repeat=a123"
+              , Err [ { col = 8, problem = P.ExpectingInt, row = 1 } ]
+              )
+            , ( "repeat=\"123\""
+              , Err [ { col = 8, problem = P.ExpectingInt, row = 1 } ]
+              )
+            , ( "repeat='123'"
+              , Err [ { col = 8, problem = P.ExpectingInt, row = 1 } ]
+              )
+            , ( "repeat=4"
+              , Ok (Repeat 4)
+              )
+            , ( "repeat=4."
+              , Err [ { col = 8, problem = P.ExpectingInt, row = 1 } ]
+              )
+            , ( "repeat=4.5"
+              , Err [ { col = 8, problem = P.ExpectingInt, row = 1 } ]
+              )
+            ]
 
 
 parser : Parser SpecialCode
@@ -61,6 +112,7 @@ parser =
         checkElements : String -> Parser SpecialCode
         checkElements str =
             let
+                -- TODO: use P.oneOf [codeA, codeB, etc.]
                 keysAndValues : List (List String)
                 keysAndValues =
                     String.split "," str
@@ -71,7 +123,7 @@ parser =
             case keysAndValues of
                 [ [ "alert", alertMsg ], [ "always", yn ], [ "repeat", nStr ] ] ->
                     Maybe.withDefault (P.problem "One or more invalid values (or keys)") <|
-                        Maybe.map2 (\num bool -> P.succeed (SpecialCode num alertMsg bool))
+                        Maybe.map2 (\num bool -> P.succeed (SpecialCode (Repeat num) alertMsg bool))
                             (String.toInt nStr)
                             (case yn of
                                 "YES" ->
@@ -111,7 +163,7 @@ specialCodeParser =
                 P.run parser "specialCode[repeat=3,alert=Some limit has been reached,always=YES]"
                     |> Expect.equal
                         (Ok
-                            { repeat = 3
+                            { repeat = Repeat 3
                             , notice = "Some limit has been reached"
                             , alwaysRun = True
                             }
@@ -132,7 +184,7 @@ specialCodeParser =
                 P.run parser "specialCode[ repeat =4, alert = Some limit has been reached,  always= YES  ]"
                     |> Expect.equal
                         (Ok
-                            { repeat = 4
+                            { repeat = Repeat 4
                             , notice = "Some limit has been reached"
                             , alwaysRun = True
                             }
@@ -142,7 +194,7 @@ specialCodeParser =
                 P.run parser "specialCode[alert=Reached bottom,repeat=5,always=NO]"
                     |> Expect.equal
                         (Ok
-                            { repeat = 5
+                            { repeat = Repeat 5
                             , notice = "Reached bottom"
                             , alwaysRun = False
                             }
