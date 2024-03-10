04edee7ac247ede6aa7957cfb60c7396b632ae98
Benjamin <benjamin.guy.thomas@gmail.com>
2024-03-10 20:34:59 +0100

    Add Ruby solution

---
+++ leetcode/LeetCode_75/merge-strings-alternately/ruby/solution.rb
@@ -0,0 +1,42 @@
+# echo ./solution.rb | entr -c ruby /_
+
+def pad(arr, final_len)
+  pad_len = final_len - arr.length
+  if pad_len > 0 then
+    arr.concat([nil] * pad_len)
+  else
+    arr
+  end
+end
+
+def interleaved(word1, word2)
+  largest_len = word1.length > word2.length ? word1.length : word2.length
+  chars1 = pad(word1.chars, largest_len)
+  chars2 = pad(word2.chars, largest_len)
+
+  chars1.zip(chars2)
+    .flatten
+    .reject(&:nil?)
+    .join
+end
+
+
+require 'minitest/autorun'
+class TestSolution < Minitest::Test
+
+  def test_padding
+   assert_equal([1,2,3,nil,nil], pad([1,2,3], 5))
+   assert_equal([1,2,3], pad([1,2,3], 3))
+   assert_equal([1,2,3], pad([1,2,3], 0))
+   assert_equal([], pad([], 0))
+   assert_equal([nil,nil,nil], pad([], 3))
+  end
+
+  def test_solution
+    assert_equal("ABCDEF", interleaved("AC", "BDEF"))
+    assert_equal("ABCDEF", interleaved("ACEF", "BD"))
+    assert_equal("ABCD", interleaved("AC", "BD"))
+    assert_equal("", interleaved("", ""))
+  end
+
+end
