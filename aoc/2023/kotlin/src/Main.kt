c1c7267f62acf356a1ae9f8ac237fc7f28484d06
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-03-31 20:06:00 +0200

    Add Kotlin setup

---
+++ aoc/2023/kotlin/src/Main.kt
@@ -0,0 +1,5 @@
+fun add(a: Int, b: Int): Int = a + b
+
+fun main() {
+    println("1 + 2 = ${add(1, 2)}")
+}
---
+++ aoc/2023/kotlin/test/MainKtTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainKtTest {
+
+    @Test
+    fun add() {
+        assertEquals(3, add(1,2))
+    }
+}
