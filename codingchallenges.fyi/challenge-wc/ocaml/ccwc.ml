b8155cae401900e0d8903c12d936ad16576a2c34
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-06-02 00:21:01 +0200

    Handle defaults

--- codingchallenges.fyi/challenge-wc/ocaml/ccwc.ml
+++ codingchallenges.fyi/challenge-wc/ocaml/ccwc.ml
@@ -10,50 +10,33 @@ let usage =
   |> Dedent.string
 ;;
 
-module Switch = struct
-  type t =
-    | Count_bytes
-    | Count_lines
-    | Count_words
-    | Count_multi_bytes_chars
-
-  let of_string = function
-    | "-c" -> Some Count_bytes
-    | "-l" -> Some Count_lines
-    | "-w" -> Some Count_words
-    | "-m" -> Some Count_multi_bytes_chars
-    | _ -> None
-  ;;
-end
-
-let run filepath = function
-  | Switch.Count_bytes ->
-    let bytes_count = Lib.count_bytes filepath in
-    Printf.printf "%d %s\n" bytes_count filepath
-  | Switch.Count_lines ->
-    let lines_count = Lib.count_lines filepath in
-    Printf.printf "%d %s\n" lines_count filepath
-  | Switch.Count_words ->
-    let words_count = Lib.count_words filepath in
-    Printf.printf "%d %s\n" words_count filepath
-  | Switch.Count_multi_bytes_chars ->
-    let chars_count = Lib.count_multi_bytes_chars filepath in
-    Printf.printf "%d %s\n" chars_count filepath
-;;
-
 let () = print_newline ()
 
 let () =
-  let (option, filepath) =
-    try
-      let option = Sys.argv.(1) |> Switch.of_string |> Option.get in
-      let filepath = Sys.argv.(2) in
-      (option, filepath)
-    with
-    | _ ->
-      ()
-      ; prerr_endline usage
-      ; exit 1
-  in
-  run filepath option
+  match Sys.argv with
+  | [| _; "-c"; filepath |] ->
+    let bytes_count = Lib.count_bytes filepath in
+    Printf.printf "%d %s\n" bytes_count filepath
+  | [| _; "-l"; filepath |] ->
+    let lines_count = Lib.count_lines filepath in
+    Printf.printf "%d %s\n" lines_count filepath
+  | [| _; "-w"; filepath |] ->
+    let words_count = Lib.count_words filepath in
+    Printf.printf "%d %s\n" words_count filepath
+  | [| _; "-m"; filepath |] ->
+    let chars_count = Lib.count_multi_bytes_chars filepath in
+    Printf.printf "%d %s\n" chars_count filepath
+  | [| _; filepath |] ->
+    let bytes_count = Lib.count_bytes filepath in
+    let lines_count = Lib.count_lines filepath in
+    let words_count = Lib.count_words filepath in
+    let chars_count = Lib.count_multi_bytes_chars filepath in
+    Printf.printf
+      "%d %d %d %d %s\n"
+      bytes_count
+      lines_count
+      words_count
+      chars_count
+      filepath
+  | _ -> prerr_endline usage
 ;;
