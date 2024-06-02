43018c99c6a9b63d9ceb26c50b992888bae9dddb
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-06-02 13:45:20 +0200

    wc is done!

--- codingchallenges.fyi/challenge-wc/ocaml/ccwc.ml
+++ codingchallenges.fyi/challenge-wc/ocaml/ccwc.ml
@@ -14,56 +14,92 @@ let usage =
   |> Dedent.string
 ;;
 
-let count_bytes filepath ic =
-  let bytes_count = Lib.Bytes.count ic in
-  Printf.printf "%d %s\n" bytes_count filepath
-;;
-
-let count_lines filepath ic =
-  let lines_count = Lib.Lines.count ic in
-  Printf.printf "%d %s\n" lines_count filepath
-;;
-
-let count_words filepath ic =
-  let words_count = Lib.Words.count ic in
-  Printf.printf "%d %s\n" words_count filepath
-;;
-
-let count_runes filepath ic =
-  let chars_count = Lib.Runes.count ic in
-  Printf.printf "%d %s\n" chars_count filepath
-;;
-
-let count_defaults filepath =
-  let (lines_count, words_count, bytes_count) =
-    ( In_channel.with_open_bin filepath Lib.Lines.count
-    , In_channel.with_open_bin filepath Lib.Words.count
-    , In_channel.with_open_bin filepath Lib.Bytes.count )
+let count_bytes filepath =
+  let aux ic =
+    let update_counter acc line = acc + Lib.Bytes.count_line_chars line in
+    In_channel.fold_lines update_counter 0 ic
   in
-  Printf.printf "%d %d %d %s\n" lines_count words_count bytes_count filepath
+  match filepath with
+  | None -> Printf.printf "%d\n" @@ aux In_channel.stdin
+  | Some filepath ->
+    In_channel.with_open_bin filepath
+    @@ fun ic -> Printf.printf "%d %s\n" (aux ic) filepath
+;;
+
+let count_lines filepath =
+  let aux ic =
+    let update_counter acc _ = acc + 1 in
+    In_channel.fold_lines update_counter 0 ic
+  in
+  match filepath with
+  | None -> Printf.printf "%d\n" @@ aux In_channel.stdin
+  | Some filepath ->
+    In_channel.with_open_bin filepath
+    @@ fun ic -> Printf.printf "%d %s\n" (aux ic) filepath
+;;
+
+let count_words filepath =
+  let aux ic =
+    let update_counter acc line = acc + Lib.Words.count_line_words line in
+    In_channel.fold_lines update_counter 0 ic
+  in
+  match filepath with
+  | None -> Printf.printf "%d\n" @@ aux In_channel.stdin
+  | Some filepath ->
+    In_channel.with_open_bin filepath
+    @@ fun ic -> Printf.printf "%d %s\n" (aux ic) filepath
+;;
+
+let count_runes filepath =
+  let aux ic =
+    let update_counter acc line = acc + Lib.Runes.count_line_runes line in
+    In_channel.fold_lines update_counter 0 ic
+  in
+  match filepath with
+  | None -> Printf.printf "%d\n" @@ aux In_channel.stdin
+  | Some filepath ->
+    In_channel.with_open_bin filepath
+    @@ fun ic -> Printf.printf "%d %s\n" (aux ic) filepath
 ;;
 
 let debug filepath =
-  In_channel.with_open_bin filepath
-  @@ fun ic ->
-  let update_counters (lines_count, words_count, bytes_count, runes_count) line =
-    ( lines_count + 1
-    , words_count + Lib.Words.count_line_words line
-    , bytes_count + Lib.Bytes.count_line_chars line
-    , runes_count + Lib.Runes.count_line_runes line )
-  in
-  let (lines_count, words_count, bytes_count, runes_count) =
+  let aux ic =
+    let update_counters (lines_count, words_count, bytes_count, runes_count) line =
+      ( lines_count + 1
+      , words_count + Lib.Words.count_line_words line
+      , bytes_count + Lib.Bytes.count_line_chars line
+      , runes_count + Lib.Runes.count_line_runes line )
+    in
     In_channel.fold_lines update_counters (0, 0, 0, 0) ic
   in
-  let want = "lines=7145\twords=58164\tbytes=342190\trunes=339292\tfile=../test.txt" in
-  let got =
-    Printf.sprintf
-      "lines=%d\twords=%d\tbytes=%d\trunes=%d\tfile=%s"
-      lines_count
-      words_count
-      bytes_count
-      runes_count
-      filepath
+  let (want, got) =
+    match filepath with
+    | None ->
+      let (lines_count, words_count, bytes_count, runes_count) = aux In_channel.stdin in
+      ( "lines=7145\twords=58164\tbytes=342190\trunes=339292"
+      , Printf.sprintf
+          "lines=%d\twords=%d\tbytes=%d\trunes=%d"
+          lines_count
+          words_count
+          bytes_count
+          runes_count )
+    | Some filepath ->
+      In_channel.with_open_bin filepath
+      @@ fun ic ->
+      let (lines_count, words_count, bytes_count, runes_count) = aux ic in
+      let want =
+        "lines=7145\twords=58164\tbytes=342190\trunes=339292\tfile=../test.txt"
+      in
+      let got =
+        Printf.sprintf
+          "lines=%d\twords=%d\tbytes=%d\trunes=%d\tfile=%s"
+          lines_count
+          words_count
+          bytes_count
+          runes_count
+          filepath
+      in
+      (want, got)
   in
   ()
   ; Printf.printf "PASS: %b\n" (want = got)
@@ -71,37 +107,45 @@ let debug filepath =
   ; Printf.printf " GOT: %s\n" got
 ;;
 
-let count_defaults_stdin () =
-  (* Wrong approach
-     Maybe I should handle one line at a time (with a Seq.t?)
-
-     So rather than consume a In_channel, I should consume a `line Seq.t`
-     First I should try to use `In_channel.input_line ic` every where (don't consume single chars)
-  *)
-  let (lines_count, words_count) =
-    (Lib.Lines.count In_channel.stdin, Lib.Words.count In_channel.stdin)
+let count_defaults filepath =
+  let aux ic =
+    let update_counters (lines_count, words_count, bytes_count) line =
+      ( lines_count + 1
+      , words_count + Lib.Words.count_line_words line
+      , bytes_count + Lib.Bytes.count_line_chars line )
+    in
+    In_channel.fold_lines update_counters (0, 0, 0) ic
   in
-  (* let () = In_channel.seek stdin 0L in *)
-  Printf.printf "%d %d\n" lines_count words_count
+  match filepath with
+  | None ->
+    let (lines_count, words_count, bytes_count) = aux In_channel.stdin in
+    Printf.printf "%d %d %d\n" lines_count words_count bytes_count
+  | Some filepath ->
+    In_channel.with_open_bin filepath
+    @@ fun ic ->
+    let (lines_count, words_count, bytes_count) = aux ic in
+    Printf.printf "%d %d %d %s\n" lines_count words_count bytes_count filepath
 ;;
 
 let run is_piped_into =
   let when_normal () =
     match Sys.argv with
-    | [| _; "-c"; filepath |] -> In_channel.with_open_bin filepath (count_bytes filepath)
-    | [| _; "-l"; filepath |] -> In_channel.with_open_bin filepath (count_lines filepath)
-    | [| _; "-w"; filepath |] -> In_channel.with_open_bin filepath (count_words filepath)
-    | [| _; "-m"; filepath |] -> In_channel.with_open_bin filepath (count_runes filepath)
-    | [| _; "-debug"; filepath |] -> debug filepath
-    | [| _; filepath |] -> count_defaults filepath
+    | [| _; "-c"; filepath |] -> count_bytes @@ Some filepath
+    | [| _; "-l"; filepath |] -> count_lines @@ Some filepath
+    | [| _; "-w"; filepath |] -> count_words @@ Some filepath
+    | [| _; "-m"; filepath |] -> count_runes @@ Some filepath
+    | [| _; "-debug"; filepath |] -> debug @@ Some filepath
+    | [| _; filepath |] -> count_defaults @@ Some filepath
     | _ -> prerr_endline usage
   in
   let when_piped_into () =
     match Sys.argv with
-    | [| _; "-c" |] -> count_bytes "" In_channel.stdin
-    | [| _; "-l" |] -> count_lines "" In_channel.stdin
-    | [| _; "-w" |] -> count_words "" In_channel.stdin
-    | [| _ |] -> count_defaults_stdin ()
+    | [| _; "-c" |] -> count_bytes None
+    | [| _; "-l" |] -> count_lines None
+    | [| _; "-w" |] -> count_words None
+    | [| _; "-m" |] -> count_runes None
+    | [| _; "-debug" |] -> debug None
+    | [| _ |] -> count_defaults None
     | _ -> failwith "todo other"
   in
   if not is_piped_into then
