772919046246bb8326f196055a6025383780d950
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-06-02 09:22:39 +0200

    Use channel as input

--- codingchallenges.fyi/challenge-wc/ocaml/ccwc.ml
+++ codingchallenges.fyi/challenge-wc/ocaml/ccwc.ml
@@ -12,31 +12,60 @@ let usage =
 
 let () = print_newline ()
 
-let () =
+let count_bytes filepath ic =
+  let bytes_count = Lib.Bytes.count ic in
+  Printf.printf "%d %s\n" bytes_count filepath
+;;
+
+let count_lines filepath ic =
+  let lines_count = Lib.Lines.count ic in
+  Printf.printf "%d %s\n" lines_count filepath
+;;
+
+let count_words filepath ic =
+  let words_count = Lib.Words.count ic in
+  Printf.printf "%d %s\n" words_count filepath
+;;
+
+let count_runes filepath ic =
+  let chars_count = Lib.Runes.count ic in
+  Printf.printf "%d %s\n" chars_count filepath
+;;
+
+let count_all filepath =
+  let (bytes_count, lines_count, words_count, chars_count) =
+    ( In_channel.with_open_bin filepath Lib.Bytes.count
+    , In_channel.with_open_bin filepath Lib.Lines.count
+    , In_channel.with_open_bin filepath Lib.Words.count
+    , In_channel.with_open_bin filepath Lib.Runes.count )
+  in
+  Printf.printf
+    "%d %d %d %d %s\n"
+    bytes_count
+    lines_count
+    words_count
+    chars_count
+    filepath
+;;
+
+let run () =
   match Sys.argv with
-  | [| _; "-c"; filepath |] ->
-    let bytes_count = Lib.count_bytes filepath in
-    Printf.printf "%d %s\n" bytes_count filepath
-  | [| _; "-l"; filepath |] ->
-    let lines_count = Lib.count_lines filepath in
-    Printf.printf "%d %s\n" lines_count filepath
-  | [| _; "-w"; filepath |] ->
-    let words_count = Lib.count_words filepath in
-    Printf.printf "%d %s\n" words_count filepath
-  | [| _; "-m"; filepath |] ->
-    let chars_count = Lib.count_multi_bytes_chars filepath in
-    Printf.printf "%d %s\n" chars_count filepath
-  | [| _; filepath |] ->
-    let bytes_count = Lib.count_bytes filepath in
-    let lines_count = Lib.count_lines filepath in
-    let words_count = Lib.count_words filepath in
-    let chars_count = Lib.count_multi_bytes_chars filepath in
-    Printf.printf
-      "%d %d %d %d %s\n"
-      bytes_count
-      lines_count
-      words_count
-      chars_count
-      filepath
+  | [| _; "-c"; filepath |] -> In_channel.with_open_bin filepath (count_bytes filepath)
+  | [| _; "-l"; filepath |] -> In_channel.with_open_bin filepath (count_lines filepath)
+  | [| _; "-w"; filepath |] -> In_channel.with_open_bin filepath (count_words filepath)
+  | [| _; "-m"; filepath |] -> In_channel.with_open_bin filepath (count_runes filepath)
+  | [| _; filepath |] -> count_all filepath
   | _ -> prerr_endline usage
 ;;
+
+let is_piped_into = not @@ Unix.isatty Unix.stdin
+
+let () =
+  Printf.printf
+    "%d => %b [%s]\n"
+    (Unix.getpid ())
+    is_piped_into
+    (Array.to_list Sys.argv |> List.tl |> String.concat "|")
+;;
+
+let () = run ()
--- codingchallenges.fyi/challenge-wc/ocaml/lib/lib.ml
+++ codingchallenges.fyi/challenge-wc/ocaml/lib/lib.ml
@@ -1,159 +1,197 @@
-let[@warning "-32"] count_bytes path =
-  let count = ref 0 in
-  In_channel.with_open_bin path
-  @@ fun ic ->
-  let () =
-    try
-      while true do
-        let _ = input_char ic in
-        count := !count + 1
-      done
-    with
-    | End_of_file -> ()
-  in
-  !count
-;;
+module Bytes = struct
+  let[@warning "-32"] count ic =
+    let count = ref 0 in
+    let () =
+      try
+        while true do
+          let _ = input_char ic in
+          count := !count + 1
+        done
+      with
+      | End_of_file -> ()
+    in
+    !count
+  ;;
 
-let count_bytes path =
-  let rec aux n ic =
-    match In_channel.input_char ic with
-    | None -> n
-    | Some _ -> aux (n + 1) ic
-  in
-  In_channel.with_open_bin path (aux 0)
-;;
+  let count ic =
+    let rec aux n =
+      match In_channel.input_char ic with
+      | None -> n
+      | Some _ -> aux (n + 1)
+    in
+    aux 0
+  ;;
+end
 
-let count_lines path =
-  let rec aux n ic =
-    try
-      ()
-      ; input_line ic |> ignore
-      ; aux (n + 1) ic
-    with
-    | End_of_file -> n
-  in
-  In_channel.with_open_bin path (aux 0)
-;;
+module Lines = struct
+  let[@warning "-32"] count ic =
+    let rec aux n =
+      try
+        ()
+        ; input_line ic |> ignore
+        ; aux (n + 1)
+      with
+      | End_of_file -> n
+    in
+    aux 0
+  ;;
+
+  let[@warning "-32"] count ic =
+    let rec aux n =
+      match In_channel.input_line ic with
+      | None -> n
+      | Some _ -> aux (n + 1)
+    in
+    aux 0
+  ;;
+
+  (* Just for fun: I prefer the version above *)
+  let count ic =
+    let rec aux n =
+      In_channel.input_line ic
+      |> Option.map (fun _ -> aux (n + 1))
+      |> Option.value ~default:n
+    in
+    aux 0
+  ;;
+end
 
 let is_white_space = function
   | ' ' | '\r' | '\n' | '\t' -> true
   | _ -> false
 ;;
 
-let[@warning "-32"] count_line_words line =
-  let len = String.length line in
-  let word_count = ref 0 in
-  let in_word = ref false in
-  ()
-  ; for i = 0 to len - 1 do
-      let curr = line.[i] in
-      if is_white_space curr then
-        in_word := false
-      else if not !in_word then (
-        ()
-        ; in_word := true
-        ; incr word_count
-      )
-    done
-  ; !word_count
-;;
+module Words = struct
+  let[@warning "-32"] count_line_words line =
+    let len = String.length line in
+    let word_count = ref 0 in
+    let in_word = ref false in
+    let () =
+      for i = 0 to len - 1 do
+        let curr = line.[i] in
+        if is_white_space curr then
+          in_word := false
+        else if not !in_word then (
+          ()
+          ; in_word := true
+          ; incr word_count
+        )
+      done
+    in
+    !word_count
+  ;;
 
-let%test_module "update_count" =
-  (module struct
+  let count_line_words line =
     let update_count (count, in_word) ch =
       if is_white_space ch then
         (count, false)
-      else if in_word then
-        (count, true)
-      else
+      else if not in_word then
         (count + 1, true)
-    ;;
+      else
+        (count, true)
+    in
+    fst (String.fold_left update_count (0, false) line)
+  ;;
 
-    open Base
+  let[@warning "-32"] count ic =
+    let rec aux n =
+      try
+        let line = input_line ic in
+        let words = count_line_words line in
+        aux (n + words)
+      with
+      | End_of_file -> n
+    in
+    aux 0
+  ;;
 
-    (* LEFT: the current count, RIGHT: whether we are in a word *)
+  let count ic =
+    let rec aux n =
+      match In_channel.input_line ic with
+      | None -> n
+      | Some line -> aux (n + count_line_words line)
+    in
+    aux 0
+  ;;
 
-    let%test_unit "whitespace, no change if already in word" =
-      [%test_eq: int * bool] (update_count (0, false) ' ') (0, false)
-    ;;
+  (* TESTING *)
+  let%test_module "update_count" =
+    (module struct
+      let update_count (count, in_word) ch =
+        if is_white_space ch then
+          (count, false)
+        else if in_word then
+          (count, true)
+        else
+          (count + 1, true)
+      ;;
 
-    let%test_unit "whitespace, transition out of word" =
-      [%test_eq: int * bool] (update_count (1, true) ' ') (1, false)
-    ;;
+      open Base
 
-    let%test_unit "non-whitespace, no change if already in word" =
-      [%test_eq: int * bool] (update_count (1, true) 'B') (1, true)
-    ;;
+      (* LEFT: the current count, RIGHT: whether we are in a word *)
 
-    let%test_unit "non-whitespace, increase count + transition to in_word if not in word" =
-      [%test_eq: int * bool] (update_count (0, false) 'A') (1, true)
-    ;;
-  end)
-;;
+      let%test_unit "whitespace, no change if already in word" =
+        [%test_eq: int * bool] (update_count (0, false) ' ') (0, false)
+      ;;
 
-let count_line_words line =
-  let update_count (count, in_word) ch =
-    if is_white_space ch then
-      (count, false)
-    else if not in_word then
-      (count + 1, true)
-    else
-      (count, true)
-  in
-  fst (String.fold_left update_count (0, false) line)
-;;
+      let%test_unit "whitespace, transition out of word" =
+        [%test_eq: int * bool] (update_count (1, true) ' ') (1, false)
+      ;;
 
-let%test_unit _ =
-  ()
-  ; [%test_eq: Base.int] 0 (count_line_words @@ "")
-  ; [%test_eq: Base.int] 0 (count_line_words @@ "   ")
-  ; [%test_eq: Base.int] 1 (count_line_words @@ "x")
-  ; [%test_eq: Base.int] 1 (count_line_words @@ "xy")
-  ; [%test_eq: Base.int] 0 (count_line_words @@ " ")
-  ; [%test_eq: Base.int] 1 (count_line_words @@ " x")
-  ; [%test_eq: Base.int] 0 (count_line_words @@ "  ")
-  ; [%test_eq: Base.int] 1 (count_line_words @@ "  x")
-  ; [%test_eq: Base.int] 1 (count_line_words @@ "  xy")
-  ; [%test_eq: Base.int] 1 (count_line_words @@ "  xyz")
-  ; [%test_eq: Base.int] 1 (count_line_words @@ "  xyz ")
-  ; [%test_eq: Base.int] 2 (count_line_words @@ "  xyz a")
-  ; [%test_eq: Base.int] 3 (count_line_words @@ "  xyz a b")
-  ; [%test_eq: Base.int] 3 (count_line_words @@ "xyz a b")
-  ; [%test_eq: Base.int] 2 (count_line_words @@ "x y")
-  ; [%test_eq: Base.int] 3 (count_line_words @@ "x y z")
-  ; [%test_eq: Base.int] 3 (count_line_words @@ "  x y z")
-  ; [%test_eq: Base.int] 3 (count_line_words @@ " x   yyy    z ")
-;;
+      let%test_unit "non-whitespace, no change if already in word" =
+        [%test_eq: int * bool] (update_count (1, true) 'B') (1, true)
+      ;;
 
-let count_words path =
-  let rec aux n ic =
-    try
-      let line = input_line ic in
-      let words = count_line_words line in
-      aux (n + words) ic
-    with
-    | End_of_file -> n
-  in
-  In_channel.with_open_bin path (aux 0)
-;;
+      let%test_unit "non-whitespace, increase count + transition to in_word if not in \
+                     word"
+        =
+        [%test_eq: int * bool] (update_count (0, false) 'A') (1, true)
+      ;;
+    end)
+  ;;
+
+  let%test_unit _ =
+    ()
+    ; [%test_eq: Base.int] 0 (count_line_words @@ "")
+    ; [%test_eq: Base.int] 0 (count_line_words @@ "   ")
+    ; [%test_eq: Base.int] 1 (count_line_words @@ "x")
+    ; [%test_eq: Base.int] 1 (count_line_words @@ "xy")
+    ; [%test_eq: Base.int] 0 (count_line_words @@ " ")
+    ; [%test_eq: Base.int] 1 (count_line_words @@ " x")
+    ; [%test_eq: Base.int] 0 (count_line_words @@ "  ")
+    ; [%test_eq: Base.int] 1 (count_line_words @@ "  x")
+    ; [%test_eq: Base.int] 1 (count_line_words @@ "  xy")
+    ; [%test_eq: Base.int] 1 (count_line_words @@ "  xyz")
+    ; [%test_eq: Base.int] 1 (count_line_words @@ "  xyz ")
+    ; [%test_eq: Base.int] 2 (count_line_words @@ "  xyz a")
+    ; [%test_eq: Base.int] 3 (count_line_words @@ "  xyz a b")
+    ; [%test_eq: Base.int] 3 (count_line_words @@ "xyz a b")
+    ; [%test_eq: Base.int] 2 (count_line_words @@ "x y")
+    ; [%test_eq: Base.int] 3 (count_line_words @@ "x y z")
+    ; [%test_eq: Base.int] 3 (count_line_words @@ "  x y z")
+    ; [%test_eq: Base.int] 3 (count_line_words @@ " x   yyy    z ")
+  ;;
+end
 
 (* RUNES *)
 
-let count_line_runes line =
-  let add acc _ = function
-    | `Uchar _ -> acc + 1
-    | `Malformed _bs -> failwith "Bad encoding (probably)"
-  in
-  Uutf.String.fold_utf_8 add 1 line
-;;
+module Runes = struct
+  let count_line_runes line =
+    let add acc _ = function
+      | `Uchar _ -> acc + 1
+      | `Malformed _bs -> failwith "Bad encoding (probably)"
+    in
+    Uutf.String.fold_utf_8 add 1 line
+  ;;
 
-let count_multi_bytes_chars path =
-  let rec aux n ic =
-    match In_channel.input_line ic with
-    | None -> n
-    | Some line ->
-      let count = count_line_runes line in
-      aux (n + count) ic
-  in
-  In_channel.with_open_bin path (aux 0)
-;;
+  let count ic =
+    let rec aux n =
+      match In_channel.input_line ic with
+      | None -> n
+      | Some line ->
+        let count = count_line_runes line in
+        aux (n + count)
+    in
+    aux 0
+  ;;
+end
