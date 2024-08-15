662aad66e16826f1ae4ecf6e4c2023459be2a61f
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-08-15 15:50:42 +0200

    Add Haskell example

---
+++ interpret-cmd/haskell/Main.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE GADTs #-}
+
+module Main where
+
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.State (
+    StateT,
+    execStateT,
+    get,
+    modify,
+ )
+import Data.List (intercalate)
+
+{-
+
+cabal install --lib transformers
+
+Run with:
+    echo ./Main.hs | entr -c runghc /_
+
+ -}
+
+data Cmd a where
+    Pure :: a -> Cmd a
+    Bind :: Cmd a -> (a -> Cmd b) -> Cmd b
+    InviteUser :: String -> Cmd ()
+    FetchUsers :: Cmd [String]
+    Broadcast :: String -> Cmd ()
+
+instance Functor Cmd where
+    fmap :: (a -> b) -> Cmd a -> Cmd b
+    fmap f g = Bind g (Pure . f)
+
+instance Applicative Cmd where
+    pure :: a -> Cmd a
+    pure = Pure
+
+    (<*>) :: Cmd (a -> b) -> Cmd a -> Cmd b
+    (<*>) f g = Bind f (<$> g)
+
+instance Monad Cmd where
+    (>>=) :: Cmd a -> (a -> Cmd b) -> Cmd b
+    (>>=) = Bind
+
+program :: Cmd ()
+program =
+    let broadcastOnEven users =
+            if even (length users)
+                then
+                    Broadcast "\x1b[1;36mThere are an even number of users\x1b[0m"
+                else
+                    Pure ()
+     in do
+            Broadcast "Booting up..."
+            users <- FetchUsers
+            broadcastOnEven users
+
+            InviteUser "@John"
+            users <- FetchUsers
+            broadcastOnEven users
+
+            InviteUser "@Jane"
+            users <- FetchUsers
+            broadcastOnEven users
+            Broadcast "Shutting down..."
+
+data AppState = AppState
+    { stCounter :: Int
+    , stUsers :: [String]
+    }
+    deriving (Show)
+
+type AppIO a = StateT AppState IO a
+
+interpret :: Cmd a -> AppIO a
+interpret command =
+    case command of
+        Pure a -> pure a
+        Bind f g -> interpret f >>= interpret . g
+        InviteUser userName -> do
+            modify
+                ( \st ->
+                    st
+                        { stUsers = userName : stUsers st
+                        , stCounter = stCounter st + 1
+                        }
+                )
+            liftIO $ putStrLn ("You invited " <> userName <> "!")
+        FetchUsers -> do
+            users <- stUsers <$> get
+            liftIO $ putStrLn ("\x1b[1;33m" <> "==> Returning users: " <> "\x1b[0m" <> unwords users)
+            pure users
+        Broadcast val -> do
+            counter <- stCounter <$> get
+            liftIO $
+                putStrLn $
+                    mconcat
+                        [ "Broadcasting: " <> quote val <> " "
+                        , "(counter=" <> show counter <> ")"
+                        ]
+          where
+            quote v = "'" <> v <> "'"
+
+main :: IO ()
+main = do
+    let initialState =
+            AppState
+                { stUsers = ["@Bob"]
+                , stCounter = 1
+                }
+    execStateT
+        (interpret program)
+        initialState
+    pure ()
