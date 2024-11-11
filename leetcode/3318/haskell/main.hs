587a2cd392c07584eb4497674ccce00e0a026d0a
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-11-11 18:48:01 +0100

    Finalize

--- leetcode/3318/haskell/main.hs
+++ leetcode/3318/haskell/main.hs
@@ -1,5 +1,3 @@
-import Control.Arrow ((>>>))
-import Data.Function ((&))
 import Data.List qualified as List
 import Data.Map qualified as Map
 
@@ -26,20 +24,11 @@ takeExactly =
 >>> makeWindow 3 [1,2,3,4,5]
 [[1,2,3],[2,3,4],[3,4,5]]
 
->>> makeWindow2 3 [1,2,3,4,5]
-[[1,2,3],[2,3,4],[3,4,5]]
-
 >>> makeWindow 6 [1,1,2,2,3,4,2,3]
 [[1,1,2,2,3,4],[1,2,2,3,4,2],[2,2,3,4,2,3]]
 -}
-makeWindow :: (Eq t, Num t) => t -> [a] -> [[a]]
+makeWindow :: (Eq n, Num n) => n -> [a] -> [[a]]
 makeWindow n lst =
-  case takeExactly n lst of
-    [] -> []
-    window -> window : makeWindow n (drop 1 lst)
-
-makeWindow2 :: Int -> [a] -> [[a]]
-makeWindow2 n lst =
   aux [] n lst
  where
   aux acc n lst =
@@ -63,14 +52,6 @@ occurrencesDesc lst =
     $ Map.fromListWith (+) [(x, 1) | x <- lst]
 
 {- |
->>> topOccurrences 2 [1,1,2,2,3,4]
-[(2,2),(1,2)]
--}
-topOccurrences :: (Ord k, Ord v, Num v) => Int -> [k] -> [(k, v)]
-topOccurrences x lst =
-  take x $ occurrencesDesc lst
-
-{- |
 2*3 + 4*5 = 26
 >>> sumOccurrences [(2,3),(4,5)]
 26
@@ -89,16 +70,3 @@ solve :: (Ord a, Num a, Num k, Eq k) => k -> Int -> [a] -> a
 solve k x lst =
   sum $
     sumOccurrences . take x . occurrencesDesc <$> makeWindow k lst
-
-{- |
->>> solve' [1,1,2,2,3,4,2,3] 6 2
-28
-
->>> solve' [3,8,7,8,7,5] 2 2
-68
--}
-solve' :: (Ord a, Num a, Num k, Eq k) => [a] -> k -> Int -> a
-solve' lst k x =
-  makeWindow k lst
-    & map (topOccurrences x >>> sumOccurrences)
-    & sum
