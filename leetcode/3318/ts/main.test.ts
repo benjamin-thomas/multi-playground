3eae516f85f25e9fa13e9657843feb73e9c5153c
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-11-12 08:19:29 +0100

    Add TypeScript solution

---
+++ leetcode/3318/ts/main.test.ts
@@ -0,0 +1,75 @@
+import {describe, expect, test} from "vitest";
+import {occurrences, slidingWindow, solve, sumRows, topmost} from "./main";
+
+/*
+npm test watch -- --clearScreen
+*/
+
+describe('sliding window', () => {
+    test("5 elements by groups of 3", () => {
+        expect(slidingWindow([1, 2], 3)).toEqual([]);
+        expect(slidingWindow([1, 2, 3], 3)).toEqual([[1, 2, 3]]);
+        expect(slidingWindow([1, 2, 3, 4], 3)).toEqual([[1, 2, 3], [2, 3, 4]]);
+        expect(slidingWindow([1, 2, 3, 4, 5], 3)).toEqual([[1, 2, 3], [2, 3, 4], [3, 4, 5]]);
+    });
+});
+
+describe("occurrences", () => {
+    test("none", () => {
+        expect(occurrences([])).toEqual(new Map());
+    });
+
+    test("many", () => {
+        expect(occurrences([1, 2, 1, 3, 2, 1])).toEqual(
+            new Map()
+                .set(1, 3)
+                .set(2, 2)
+                .set(3, 1)
+        );
+    });
+
+    test("topmost", () => {
+        expect(topmost(2, (a, b) => b - a, new Map()
+            .set(1, 3)
+            .set(2, 2)
+            .set(3, 1)
+            .set(4, 2)
+        )).toEqual<Array<[number, number]>>([[1, 3], [4, 2]]);
+    });
+
+    test("sum", () => {
+        const input: Array<[number, number]> = [[2, 3], [4, 5]];
+
+        // 2*3 + 4*5 = 26
+        expect(sumRows(input)).toEqual(26);
+    });
+});
+
+
+describe("solve", () => {
+    /*
+    Input: nums = [1,1,2,2,3,4,2,3], k = 6, x = 2
+
+    Output: [6,10,12]
+    
+    irb(main):001:0> 6 + 10 + 12
+    => 28
+     */
+    test("example 1", () => {
+        expect(solve( [1, 1, 2, 2, 3, 4, 2, 3], 6, 2)).toEqual(28);
+    });
+    
+    /*
+    Input: nums = [3,8,7,8,7,5], k = 2, x = 2
+
+    Output: [11,15,15,15,12]
+    
+    irb(main):002:0> [11,15,15,15,12].sum
+    => 68
+     */
+    test("example 2", () => {
+        expect(solve([3,8,7,8,7,5], 2, 2)).toEqual(68)
+    });
+});
+
+
---
+++ leetcode/3318/ts/main.ts
@@ -0,0 +1,34 @@
+export function slidingWindow<T>(items: T[], size: number): T[][] {
+    const result: T[][] = [];
+    for (let i = 0; i <= items.length - size; i++) {
+        const slice = items.slice(i, i + size);
+        result.push(slice);
+    }
+    return result;
+}
+
+export function occurrences<T>(items: T[]): Map<T, number> {
+    const result = new Map<T, number>();
+    for (const item of items) {
+        const count = result.get(item) || 0;
+        result.set(item, count + 1);
+    }
+    return result;
+}
+
+export function topmost<V>(n: number, compare: (a: V, b: V) => number, dict: Map<number, V>): Array<[number, V]> {
+    return Array.from(dict.entries()).sort(([ka, va], [kb, vb]) => {
+        return va === vb ? kb - ka : compare(va, vb);
+    }).slice(0, n);
+}
+
+export function sumRows(rows: [number, number][]): number {
+    return rows.reduce((acc, [a, b]) => acc + a * b, 0);
+}
+
+export function solve(items: number[], k: number, x: number): number {
+    const compareValues = (a: number, b: number) => b - a;
+    return slidingWindow(items, k)
+        .map((window) => topmost(x, compareValues, occurrences(window)))
+        .reduce((acc, rows) => acc + sumRows(rows), 0);
+}
