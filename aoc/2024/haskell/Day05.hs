01ece9a96306310cac6c7d3ee3615359dd49ba73
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-12-22 22:41:55 +0100

    Final cleanup

--- aoc/2024/haskell/Day05.hs
+++ aoc/2024/haskell/Day05.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -Wextra #-}
 
-module Day05 (main, splitManyA, splitManyB, splitManyC) where
+module Day05 (main) where
 
 import Data.Bifunctor (Bifunctor (bimap), second)
 import Data.Bool (bool)
@@ -10,89 +10,40 @@ import Data.List (unfoldr)
 import Data.List qualified as List
 import Data.Map (Map)
 import Data.Map qualified as Map
-import Data.Maybe (fromMaybe)
+import Data.Set (Set, notMember)
+import Data.Set qualified as Set
 
-rulePass :: Map Int [Int] -> [Int] -> Bool
-rulePass rules = aux []
+rulePass :: Map Int (Set Int) -> [Int] -> Bool
+rulePass rules = aux Set.empty
   where
-    aux :: [Int] -> [Int] -> Bool
+    aux :: Set Int -> [Int] -> Bool
     aux seen remaining =
         case remaining of
             [] -> True
             (x : xs) ->
                 let
-                    isValid =
+                    notBefore = x `notMember` seen
+
+                    notAfter =
                         case Map.lookup x rules of
                             Nothing ->
-                                x `notElem` seen
+                                True
                             Just after ->
-                                x `notElem` seen && all (`notElem` seen) after
+                                all (`notMember` seen) after
                  in
-                    isValid && aux (x : seen) xs
+                    notBefore && notAfter && aux (Set.insert x seen) xs
 
 {-
 
-First "naive" version, the may overflow the stack
-
->>> splitManyA ',' "1,2,3"
+>>> splitMany ',' "1,2,3"
 ["1","2","3"]
 
->>> splitManyA ',' "1,"
-["1"]
-
->>> splitManyA ',' "1"
-["1"]
-
->>> splitManyA ',' ""
-[]
-
- -}
-
-splitManyA :: (Eq a) => a -> [a] -> [[a]]
-splitManyA _ [] = []
-splitManyA c xs = x : splitManyA c rest
-  where
-    (x, rest) = splitOnce c xs
-
-{-
-
-"Better" version, that won't overflow the stack
-
->>> splitManyB ',' "1,2,3"
-["1","2","3"]
-
->>> splitManyB ',' "1,"
-["1"]
-
->>> splitManyB ',' "1"
-["1"]
-
->>> splitManyB ',' ""
-[]
-
- -}
-splitManyB :: (Eq a) => a -> [a] -> [[a]]
-splitManyB c =
-    aux []
-  where
-    aux acc [] = reverse acc
-    aux acc xs =
-        case splitOnce c xs of
-            (x, rest) -> aux (x : acc) rest
-
-{-
-
-"Ultimate" version, as efficient as it could be.
-
->>> splitManyC ',' "1,2,3"
-["1","2","3"]
-
->>> splitManyC ',' "1,"
+>>> splitMany ',' "1,"
 ["1"]
 
  -}
-splitManyC :: (Eq a) => a -> [a] -> [[a]]
-splitManyC c =
+splitMany :: (Eq a) => a -> [a] -> [[a]]
+splitMany c =
     unfoldr $ \case
         [] -> Nothing
         xs -> Just (splitOnce c xs)
@@ -113,19 +64,24 @@ splitManyC c =
 splitOnce :: (Eq a) => a -> [a] -> ([a], [a])
 splitOnce c = second (drop 1) . break (== c)
 
--- mapM_ print $ Map.toList $ Data.List.sort <$> makeRules l
-makeRules :: [String] -> Map Int [Int]
+makeRules :: [String] -> Map Int (Set Int)
 makeRules =
     foldr
-        ( \line acc -> do
-            let (k, v) = bimap read read $ splitOnce '|' line :: (Int, Int)
-             in Map.alter (\xs -> Just (v : fromMaybe [] xs)) k acc
+        ( \line dict ->
+            let (key :: Int, val :: Int) = bimap read read $ splitOnce '|' line
+             in Map.alter
+                    ( \case
+                        Nothing -> Just (Set.singleton val)
+                        Just xs -> Just (Set.insert val xs)
+                    )
+                    key
+                    dict
         )
         Map.empty
 
 makeUpdates :: [String] -> [[Int]]
 makeUpdates lst =
-    fmap read . splitManyC ',' <$> lst
+    fmap read . splitMany ',' <$> lst
 
 middle :: [a] -> a
 middle lst = lst !! (length lst `div` 2)
@@ -138,33 +94,17 @@ middle lst = lst !! (length lst `div` 2)
 [Left 1,Right 2,Left 3]
 
  -}
-applyRules :: (Map Int [Int], [[Int]]) -> [Either [Int] [Int]]
+applyRules :: (Map Int (Set Int), [[Int]]) -> [Either [Int] [Int]]
 applyRules (rules, updates) =
-    -- fmap
-    --     ( \update ->
-    --         if rulePass rules update
-    --             then
-    --                 Right update
-    --             else
-    --                 Left update
-    --     )
-    --     updates
-    -- fmap (\update -> bool (Left update) (Right update) (rulePass rules update)) updates
-    -- fmap (bool <$> Left <*> Right <*> rulePass rules) updates
     bool <$> Left <*> Right <*> rulePass rules <$> updates
-
-ruleToNum :: Either [Int] [Int] -> Either [Int] Int
-ruleToNum =
-    \case
-        Right xs -> Right $ middle xs
-        Left xs -> Left xs
 
 answer1 :: String -> Int
 answer1 input =
     let (rules, updates) = bimap makeRules makeUpdates $ splitOnce [] $ lines input
-     in sum $ rights (ruleToNum <$> applyRules (rules, updates))
+        good = rights $ applyRules (rules, updates)
+     in sum $ middle <$> good
 
-reorder :: Map Int [Int] -> [Int] -> [Int]
+reorder :: Map Int (Set Int) -> [Int] -> [Int]
 reorder rules = aux []
   where
     aux :: [Int] -> [Int] -> [Int]
