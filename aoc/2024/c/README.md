bc29c2b427b089a0281149734a0fba53f4270eab
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2025-01-05 20:02:24 +0100

    Add AOC day 1 in C

--- aoc/2024/c/README.md
+++ aoc/2024/c/README.md
@@ -23,8 +23,8 @@ TARGET=prod cmake -S . -B build
 cmake --build ./build
 
 # Rebuild fully
-rm -r ./build && cmake -S . -B ./build && cmake --build ./build
-rm -r ./build && TARGET=prod cmake -S . -B ./build && cmake --build ./build
+rm -rf ./build && cmake -S . -B ./build && cmake --build ./build
+rm -rf ./build && TARGET=prod cmake -S . -B ./build && cmake --build ./build
 ```
 
 ## vscode tips
@@ -38,3 +38,69 @@ rm -r ./build && TARGET=prod cmake -S . -B ./build && cmake --build ./build
   - rg --files | entr -c cmake --build ./build
 - continuously lint (terminal 2):
   - rg --files -t c | entr -c clang-tidy /_
+
+---
+
+## GDB tricks
+
+Use enter to repeat the last command
+
+- br 14 # break a tile 14
+- c # short for continue
+- enter
+- enter
+- enter
+
+When in a loop, use `display` to print many variables on break
+
+```
+display {i, size} # shows 2 (related) values
+display {xs[i], ys[i]} # shows 2 (related) array values
+p *ys@size # display array values
+p *xs@3 # show the first 3 array values
+```
+
+---
+
+Remove break points
+
+- info br
+  - del NUM
+
+---
+
+Remove display value(s)
+
+- display
+- undisplay NUM
+
+---
+
+Reload file (after recompilation)
+
+- gdb ./my_binary
+- set confirm off # remove annoying confirmation
+- file ./my_binary # reload
+
+---
+
+Find the source of a segfault
+
+- enter gdb
+- run
+- see the segfault
+- bt
+- observe the last executed line
+
+---
+
+Always execute from the debugger
+
+- gdb ./my_binary
+- define go
+  - !clear
+  - file ./my_binary # always reload
+  - run # then run
+  - end
+- echo "Then from now on, just execute 'go'"
+- go
--- aoc/2024/c/day01.c
+++ aoc/2024/c/day01.c
@@ -1,35 +1,99 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// #include "./utils.h"
 
 /*
 
-gcc -g -Wall -Wextra -pedantic day01.c && valgrind a.out
-rg --files | entr -rc bash -c 'gcc -g -Wall -Wextra -pedantic day01.c -o /tmp/tmp && valgrind /tmp/tmp'
+Terminal 1:
+  echo ./day01.c | entr -c bash -c 'clang-tidy ./day01.c && cmake --build ./build'
+  # Or
+  rg --files -t c | entr -c bash -c 'clang-tidy ./day01.c && cmake --build ./build'
 
+Terminal 2:
+  echo build/day01 | entr -c /_
+  # or just reload via gdb
 */
 
-void work(void) {
-  int arr[5];
-  arr[4] = 10;  // Invalid access2
+int compare_int(const void* a, const void* b) { return *(int*)a - *(int*)b; }
 
-  printf("arr len is: %lu\n", sizeof(arr));
+#define seen_max_val 0xFF << 9
 
-  char *ptr = malloc(10);
-  if (!ptr) {
-    printf("Failed to allocate memory\n");
-    return;
+void solve(FILE* file) {
+  int seen[seen_max_val] = {0};
+  char line[32];
+  int capacity = 0xFF;
+
+  int* xs = malloc(capacity * sizeof(int));
+  int* ys = malloc(capacity * sizeof(int));
+  assert(xs && ys);
+
+  int distances = 0;
+  int similarities = 0;
+  {
+    int size = 0;
+    while (fgets(line, sizeof(line), file)) {
+      // Grow array capacity if necessary
+      if (size >= capacity) {
+        capacity = capacity << 1;
+        xs = realloc(xs, capacity * sizeof(int));
+        ys = realloc(ys, capacity * sizeof(int));
+        assert(xs && ys);
+      }
+
+      // Parse left/right numbers
+      int left, right;
+      char* token = strtok(line, " ");
+      left = atoi(token);
+      token = strtok(NULL, " ");
+      right = atoi(token);
+
+      assert(right >= 0);  // sanity check
+      assert(right <= seen_max_val &&
+             "Add one more bit shift if this is triggered");
+
+      // Mark right number seen
+      seen[right]++;
+
+      xs[size] = left;
+      ys[size] = right;
+      size++;  // track the actual size as we go, need it later.
+    }
+
+    qsort(xs, size, sizeof(int), compare_int);
+    qsort(ys, size, sizeof(int), compare_int);
+
+    for (int i = 0; i < size; i++) {
+      distances += abs(ys[i] - xs[i]);
+    }
+
+    for (int i = 0; i < size; i++) {
+      similarities += xs[i] * seen[xs[i]];
+    }
   }
-  free(ptr);
-  printf("wat");
+
+  // %12 left-pads with up to 12 spaces
+  printf("Part 1 (total distances):    %12d\t(OK=%d)\n", distances,
+         distances == 1189304);
+  printf("Part 2 (total similarities): %12d\t(OK=%d)\n", similarities,
+         similarities == 24349736);
+
+  free(ys);
+  free(xs);
 }
 
 int main(void) {
-  int i = 0;
-
-  while (i < 10) {
-    i++;
-    printf("hello2\n");
+  const char* filename = "../_inputs/01.txt";
+  FILE* file = fopen(filename, "r");
+  if (!file) {
+    printf("Failed to open file: %s\n", filename);
+    exit(1);
   }
 
-  work();
+  solve(file);
+
+  fclose(file);
+  return 0;
 }
