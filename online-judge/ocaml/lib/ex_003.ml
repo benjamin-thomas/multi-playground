f7c3401eba9dfcb267d1acc8d22515fa540ce937
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2025-03-02 15:34:39 +0100

    Fix OCaml solution based on Haskell's

--- online-judge/ocaml/lib/ex_003.ml
+++ online-judge/ocaml/lib/ex_003.ml
@@ -4,12 +4,85 @@
       dune exec ./test/test.exe test 003 -w
 *)
 
+let find_offset str =
+  String.index_opt str '.'
+  |> Option.map (fun pos -> String.length str - pos)
+  |> Option.value ~default:0
+;;
+
+let remove_leading_zeros str =
+  let len = String.length str in
+  let (start_at, _) =
+    String.fold_left
+      begin
+        fun (cursor, stop) c ->
+          if stop || c <> '0' then
+            (cursor, true)
+          else
+            (cursor + 1, false)
+      end
+      (0, false)
+      str
+  in
+  String.sub str start_at (len - start_at)
+;;
+
+exception Break
+
+let remove_leading_zeros2 str =
+  let len = String.length str in
+  let idx =
+    let idx = ref 0 in
+    try
+      for i = 0 to len - 1 do
+        if str.[i] <> '0' then
+          raise Break
+        else
+          idx := i + 1
+      done;
+      !idx
+    with
+    | Break -> !idx
+  in
+  String.sub str idx (len - idx)
+;;
+
 let rev x =
   let len = String.length x in
   String.init len (fun n -> String.get x (len - n - 1))
 ;;
 
+let insert_separator offset str =
+  let chars =
+    snd
+    @@ String.fold_right
+         begin
+           fun c (n, acc) ->
+             ( n + 1
+             , c
+               ::
+               (if n > offset && n mod 3 = offset mod 3 then
+                  ',' :: acc
+                else
+                  acc) )
+         end
+         str
+         (0, [])
+  in
+  let buf = Buffer.create 1024 in
+  List.iter (Buffer.add_char buf) chars;
+  Buffer.contents buf
+;;
+
 let solution str =
+  let str2 = remove_leading_zeros str in
+  if String.starts_with ~prefix:"." str2 then
+    str
+  else
+    insert_separator (find_offset str2) str2
+;;
+
+let solution_bad str =
   let n = ref 0 in
   let buf = Buffer.create 1024 in
   for i = String.length str - 1 downto 0 do
--- online-judge/ocaml/test/test.ml
+++ online-judge/ocaml/test/test.ml
@@ -2,53 +2,61 @@
    dune exec ./test/test.exe test 001 -w
 *)
 
-let others () =
-  let e = epsilon_float in
-  ();
-  Alcotest.(check @@ float e) "0 is 0" 0. 0.;
-  Alcotest.(check @@ float e) "0 is epsilon" 0. e;
-  Alcotest.(check @@ neg @@ float e) "0 is not 1" 0. 1.;
-  Alcotest.(check @@ neg @@ float e) "1 is not 0" 1. 0.;
-  Alcotest.(check @@ float e) ".3 is .3" (0.1 +. 0.2) 0.3
-;;
+module T001 = struct
+  module M = Lib.Ex_001
 
-module Ex_001 = Lib.Ex_001
+  let solution () =
+    let assert_eq = Alcotest.(check int) "" in
+    assert_eq 4 @@ M.solution [ 1; 1; 2; 2; 3; 3; 4; 5; 5 ];
+    assert_eq 2 @@ M.solution [ 0; 1; 0; 1; 2 ];
+    assert_eq 10 @@ M.solution [ 7; 3; 3; 7; 10 ];
+    ()
+  ;;
+end
 
-let ex_001_tests () =
-  let assert_eq = Alcotest.(check int) "" in
-  ();
-  assert_eq 4 @@ Ex_001.solution [ 1; 1; 2; 2; 3; 3; 4; 5; 5 ];
-  assert_eq 2 @@ Ex_001.solution [ 0; 1; 0; 1; 2 ];
-  assert_eq 10 @@ Ex_001.solution [ 7; 3; 3; 7; 10 ]
-;;
+module T003 = struct
+  module M = Lib.Ex_003
 
-module Ex_003 = Lib.Ex_003
+  let find_offset () =
+    let assert_eq = Alcotest.(check int) "" in
+    assert_eq 0 @@ M.find_offset "1234";
+    assert_eq 1 @@ M.find_offset "1234.";
+    assert_eq 2 @@ M.find_offset "1234.2";
+    assert_eq 3 @@ M.find_offset "1234.23";
+    ()
+  ;;
 
-let ex_003_tests () =
-  (*{|
+  let remove_leading_zeros () =
+    let assert_eq = Alcotest.(check string) "" in
+    assert_eq "1234" @@ M.remove_leading_zeros "00001234";
+    assert_eq "1234" @@ M.remove_leading_zeros "1234";
+    assert_eq "" @@ M.remove_leading_zeros "";
+    assert_eq "wat" @@ M.remove_leading_zeros "wat";
+    assert_eq "12300" @@ M.remove_leading_zeros "0012300";
+    ()
+  ;;
 
-  inspect!(solution("1294512.12412"), content="1,294,512.12412")
-  inspect!(solution("0000123456789.99"), content="123,456,789.99")
-  inspect!(solution("987654321"), content="987,654,321")
-
-  |}*)
-  let assert_eq = Alcotest.(check string) "" in
-  assert_eq "1,294,512" @@ Ex_003.solution "1294512";
-  assert_eq "1,294,513.8" @@ Ex_003.solution "1294513.8";
-  ()
-;;
-
-(* ; assert_eq "1,294,512.12412" @@ Ex_003.solution "1294512.12412" *)
-
-(* ; assert_eq "123,456,789.99" @@ Ex_003.solution "0000123456789.99" *)
-(* ; assert_eq "987,654,321" @@ Ex_003.solution "987654321" *)
+  let solution () =
+    let assert_eq = Alcotest.(check string) "" in
+    assert_eq "0.123" @@ M.solution "0.123";
+    assert_eq "1,294,512" @@ M.solution "1294512";
+    assert_eq "1,294,513.8" @@ M.solution "1294513.8";
+    assert_eq "1,294,512.12412" @@ M.solution "1294512.12412";
+    assert_eq "123,456,789.99" @@ M.solution "0000123456789.99";
+    assert_eq "987,654,321" @@ M.solution "987654321";
+    ()
+  ;;
+end
 
 let () =
   let open Alcotest in
   run
     "Online Judge"
-    [ ("Ex001", [ ("Suite", `Quick, ex_001_tests) ])
-    ; ("Ex003", [ ("Suite", `Quick, ex_003_tests) ])
-    ; ("Other floats", [ ("others", `Quick, others) ])
+    [ ("Ex001", [ ("Solution", `Quick, T001.solution) ])
+    ; ( "Ex003"
+      , [ ("Find offset", `Quick, T003.find_offset)
+        ; ("Strip zeros", `Quick, T003.remove_leading_zeros)
+        ; ("Solution", `Quick, T003.solution)
+        ] )
     ]
 ;;
