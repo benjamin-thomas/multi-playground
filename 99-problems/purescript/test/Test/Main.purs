16caf93048edb171f7941ce9e602b3930d3b248d
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2026-06-10 21:55:01 +0200

    Add test run instructions

--- 99-problems/purescript/test/Test/Main.purs
+++ 99-problems/purescript/test/Test/Main.purs
@@ -7,6 +7,14 @@ import Test.Ex01 as Ex01
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
+{-
+
+Run the tests with:
+
+rg --files | entr -c spago test
+
+ -}
+
 main :: Effect Unit
 main = runSpecAndExitProcess [ consoleReporter ] do
   Ex01.spec
